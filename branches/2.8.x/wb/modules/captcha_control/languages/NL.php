7f3c662387fb2f2b846aa0acc69a7b31d5353dc7
aldus <aldus@98bf1eab-f5fc-0310-a3ed-aac8851f7404>
2009-09-21 11:15:13 +0000

    Dutch tranlation(-s) for the captcha-control. [Ticket: #777] (Thanks to Argos)

--- branches/2.8.x/wb/modules/captcha_control/languages/NL.php
+++ branches/2.8.x/wb/modules/captcha_control/languages/NL.php
@@ -27,35 +27,35 @@
 */
 
 // Headings and text outputs
-$MOD_CAPTCHA_CONTROL['HEADING']           = 'Captcha en ASP control';
-$MOD_CAPTCHA_CONTROL['HOWTO']             = 'Hier kan je het gedrag van "CAPTCHA" en "Advanced Spam Protection" (ASP) beheren. Om ASP te laten werken met een bepaalde module, deze speciale module moet aangepast worden om gebruik te kunnen maken van ASP.';
+$MOD_CAPTCHA_CONTROL['HEADING']				= 'CAPTCHA en Advanced Spam Protection';
+$MOD_CAPTCHA_CONTROL['HOWTO']				= 'Hier kunt u "CAPTCHA" en "Advanced Spam Protection" (ASP) beheren. Let op: alleen daarvoor aangepaste modules werken met ASP.';
 
 // Text and captions of form elements
-$MOD_CAPTCHA_CONTROL['CAPTCHA_CONF']      = 'CAPTCHA Configuratie';
-$MOD_CAPTCHA_CONTROL['CAPTCHA_TYPE']      = 'Type van CAPTCHA';
-$MOD_CAPTCHA_CONTROL['CAPTCHA_EXP']       = 'CAPTCHA settings for modules are located in the respective module settings';
-$MOD_CAPTCHA_CONTROL['USE_SIGNUP_CAPTCHA']= 'Activeer CAPTCHA om aan te melden';
-$MOD_CAPTCHA_CONTROL['ENABLED']           = 'Aan';
-$MOD_CAPTCHA_CONTROL['DISABLED']          = 'Uit';
-$MOD_CAPTCHA_CONTROL['ASP_CONF']          = 'Advanced Spam Protection Configuratie';
-$MOD_CAPTCHA_CONTROL['ASP_TEXT']          = 'Activeer ASP (indien beschikbaar)';
-$MOD_CAPTCHA_CONTROL['ASP_EXP']           = 'ASP zal proberen te detecteren of de invoer van een formulier ingegeven werd via een mens of een spam-bot.';
-$MOD_CAPTCHA_CONTROL['CALC_TEXT']         = 'Rekensom als tekst';
-$MOD_CAPTCHA_CONTROL['CALC_IMAGE']        = 'Rekensom als figuur';
-$MOD_CAPTCHA_CONTROL['CALC_TTF_IMAGE']    = 'Rekensom als figuur met verschillende lettertypen en achtergronden';
-$MOD_CAPTCHA_CONTROL['TTF_IMAGE']         = 'Figuur met verschillende lettertypen en achtergronden';
-$MOD_CAPTCHA_CONTROL['OLD_IMAGE']         = 'Oude stijl (niet aan te raden)';
-$MOD_CAPTCHA_CONTROL['TEXT']              = 'Tekst-CAPTCHA';
-$MOD_CAPTCHA_CONTROL['CAPTCHA_ENTER_TEXT']= 'Vragen en antwoorden';
-$MOD_CAPTCHA_CONTROL['CAPTCHA_TEXT_DESC'] = 'Wis dit om uw lijst in te geven'."\n".'of de wijzigingen zullen niet bewaard worden!'."\n".'### voorbeeld ###'."\n".'Hier kan je vragen en antwoorden ingeven.'."\n".'Gebruik:'."\n".'?Wat is de <b>voornaam</b> van <b>Claudia</b> Schiffer?'."\n".'!Claudia'."\n".'?Vraag 2'."\n".'!Antwoord 2'."\n".' ... '."\n".'indien de taal geen rol speelt.'."\n".''."\n".'Of, indien taal wel een rol speelt, gebruik:'."\n".'?EN:What\'s <b>Claudia</b> Schiffer\'s <b>first name</b>?'."\n".'!Claudia'."\n".'?EN:Question 2'."\n".'!Answer 2'."\n".'?DE:Wie ist der <b>Vorname</b> von <b>Claudia</b> Schiffer?'."\n".'!Claudia'."\n".' ... '."\n".'### voorbeeld ###'."\n".'';
+$MOD_CAPTCHA_CONTROL['CAPTCHA_CONF']		= 'CAPTCHA-configuratie';
+$MOD_CAPTCHA_CONTROL['CAPTCHA_TYPE']		= 'Type CAPTCHA';
+$MOD_CAPTCHA_CONTROL['CAPTCHA_EXP']			= 'Let op: CAPTCHA-instellingen voor modules bevinden zich in de desbetreffende module-instellingen.';
+$MOD_CAPTCHA_CONTROL['USE_SIGNUP_CAPTCHA']	= 'Activeer CAPTCHA voor aanmeldingen';
+$MOD_CAPTCHA_CONTROL['ENABLED']				= 'Inschakelen';
+$MOD_CAPTCHA_CONTROL['DISABLED']			= 'Uitschakelen';
+$MOD_CAPTCHA_CONTROL['ASP_CONF']			= 'Advanced Spam Protection';
+$MOD_CAPTCHA_CONTROL['ASP_TEXT']			= 'Activeer ASP (indien beschikbaar)';
+$MOD_CAPTCHA_CONTROL['ASP_EXP']				= 'ASP probeert te detecteren of het formulier ingevuld werd door een persoon of een spam-bot.';
+$MOD_CAPTCHA_CONTROL['CALC_TEXT']			= 'Rekensom als tekst';
+$MOD_CAPTCHA_CONTROL['CALC_IMAGE']			= 'Rekensom als afbeelding';
+$MOD_CAPTCHA_CONTROL['CALC_TTF_IMAGE']		= 'Rekensom als afbeelding met verschillende lettertypen en achtergronden';
+$MOD_CAPTCHA_CONTROL['TTF_IMAGE']			= 'Afbeelding met verschillende lettertypen en achtergronden';
+$MOD_CAPTCHA_CONTROL['OLD_IMAGE']			= 'Oude stijl (niet aan te raden)';
+$MOD_CAPTCHA_CONTROL['TEXT']				= 'Tekst-CAPTCHA';
+$MOD_CAPTCHA_CONTROL['CAPTCHA_ENTER_TEXT']	= 'Vragen en antwoorden';
+$MOD_CAPTCHA_CONTROL['CAPTCHA_TEXT_DESC']	= 'Verwijder dit om uw lijst in te voeren, of de wijzigingen zullen niet bewaard worden!'."\n".'### voorbeeld ###'."\n".'Hier kunt u vragen en antwoorden invoeren.'."\n".'Gebruik:'."\n".'?Wat is de <b>voornaam</b> van <b>Pietje</b> Puk?'."\n".'!Pietje'."\n".'?Vraag 2'."\n".'!Antwoord 2'."\n".' ... '."\n".'indien de taal geen rol speelt.'."\n".''."\n".'Of, indien taal wel een rol speelt, gebruik:'."\n".'?EN:What\'s <b>John</b> Doe\'s <b>first name</b>?'."\n".'!John'."\n".'?EN:Question 2'."\n".'!Answer 2'."\n".'?DE:Wie ist der <b>Vorname</b> von <b>Claudia</b> Schiffer?'."\n".'!Claudia'."\n".' ... '."\n".'### voorbeeld ###'."\n".'';
 
-$MOD_CAPTCHA['VERIFICATION']           = 'Verificatie';
-$MOD_CAPTCHA['ADDITION']               = 'plus';
-$MOD_CAPTCHA['SUBTRAKTION']            = 'min';
-$MOD_CAPTCHA['MULTIPLIKATION']         = 'maal';
-$MOD_CAPTCHA['VERIFICATION_INFO_RES']  = 'Geef het resultaat in';
-$MOD_CAPTCHA['VERIFICATION_INFO_TEXT'] = 'Geef de tekst in';
-$MOD_CAPTCHA['VERIFICATION_INFO_QUEST'] = 'Beantwoord de vraag';
-$MOD_CAPTCHA['INCORRECT_VERIFICATION'] = 'Verificatie mislukt';
+$MOD_CAPTCHA['VERIFICATION']				= 'Verificatie';
+$MOD_CAPTCHA['ADDITION']					= 'plus';
+$MOD_CAPTCHA['SUBTRAKTION']					= 'min';
+$MOD_CAPTCHA['MULTIPLIKATION']				= 'maal';
+$MOD_CAPTCHA['VERIFICATION_INFO_RES']		= 'Voer het resultaat in a.u.b.';
+$MOD_CAPTCHA['VERIFICATION_INFO_TEXT']		= 'Voer de tekst in a.u.b.';
+$MOD_CAPTCHA['VERIFICATION_INFO_QUEST']		= 'Beantwoord de vraag a.u.b.';
+$MOD_CAPTCHA['INCORRECT_VERIFICATION']		= 'Verificatie mislukt';
 
 ?>
