09e0842b7091488f586d2e5fd95f4f4da6c82165
Luisehahne <Luisehahne@98bf1eab-f5fc-0310-a3ed-aac8851f7404>
2010-01-14 14:10:09 +0000

    Ticket #877 add slovak lang in Captcha and Advanced-Spam-Protection

---
+++ wb/modules/captcha_control/languages/SK.php
@@ -0,0 +1,99 @@
+<?php
+/****************************************************************************
+* SVN Version information:
+*
+* $Id$
+*
+*
+*
+*****************************************************************************
+*                          WebsiteBaker
+*
+* WebsiteBaker Project <http://www.websitebaker2.org/>
+* Copyright (C) 2009, Website Baker Org. e.V.
+*         http://start.websitebaker2.org/impressum-datenschutz.php
+* Copyright (C) 2004-2009, Ryan Djurovich
+*
+*                        About WebsiteBaker
+*
+* Website Baker is a PHP-based Content Management System (CMS)
+* designed with one goal in mind: to enable its users to produce websites
+* with ease.
+*
+*****************************************************************************
+*
+*****************************************************************************
+*                        LICENSE INFORMATION
+*
+* WebsiteBaker is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+*
+* WebsiteBaker is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+* See the GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+****************************************************************************
+*
+*                   WebsiteBaker Extra Information
+*
+*
+*
+*
+*
+*
+*
+*****************************************************************************/
+/**
+ *
+ * @category     frontend
+ * @package      framework
+ * @author       Ryan Djurovich
+ * @copyright    2004-2009, Ryan Djurovich
+ * @copyright    2009-2010, Website Baker Org. e.V.
+ * @version      $Id$
+ * @platform     WebsiteBaker 2.8.x
+ * @requirements >= PHP 4.3.4
+ * @license      http://www.gnu.org/licenses/gpl.html
+ *
+ */
+
+// Headings and text outputs
+$MOD_CAPTCHA_CONTROL['HEADING']           = 'Captcha a ASP kontrola';
+$MOD_CAPTCHA_CONTROL['HOWTO']             = 'Tu m&ocirc;&#382;ete ovplyvni&#357; spr&aacute;vanie "CAPTCHA" a "Advanced Spam Protection" (ASP).
+						Ak chcete pou&#382;&iacute;va&#357; ASP pr&aacute;cu s dan&yacute;m modulom, tento &#353;peci&aacute;lny modul, mus&iacute; by&#357; upraven&yacute; pre vyu&#382;itie ASP.';
+
+// Text and captions of form elements
+$MOD_CAPTCHA_CONTROL['CAPTCHA_CONF']      = 'CAPTCHA Konfigur&aacute;cia';
+$MOD_CAPTCHA_CONTROL['CAPTCHA_TYPE']      = 'Typ CAPTCHA';
+$MOD_CAPTCHA_CONTROL['CAPTCHA_EXP']       = 'CAPTCHA settings for modules are located in the respective module settings';
+$MOD_CAPTCHA_CONTROL['USE_SIGNUP_CAPTCHA']= 'Aktivova&#357; CAPTCHA pre prihl&aacute;senie';
+$MOD_CAPTCHA_CONTROL['ENABLED']           = 'Povoli&#357;';
+$MOD_CAPTCHA_CONTROL['DISABLED']          = 'Zak&aacute;za&#357;';
+$MOD_CAPTCHA_CONTROL['ASP_CONF']          = 'Advanced Spam Protection Konfigur&aacute;cia';
+$MOD_CAPTCHA_CONTROL['ASP_TEXT']          = 'Aktivova&#357; ASP (ak je dostupn&eacute;)';
+$MOD_CAPTCHA_CONTROL['ASP_EXP']           = 'ASP sa sna&#382;&iacute; zisti&#357;, &#269;i forma vstupu poch&aacute;dza od &#269;loveka alebo spam-bota.';
+$MOD_CAPTCHA_CONTROL['CALC_TEXT']         = 'V&yacute;po&#269;et ako text';
+$MOD_CAPTCHA_CONTROL['CALC_IMAGE']        = 'V&yacute;po&#269;et ako obr&aacute;zok';
+$MOD_CAPTCHA_CONTROL['CALC_TTF_IMAGE']    = 'V&yacute;po&#269;et ako obr&aacute;zok s r&ocirc;znymi fontmy a pozad&iacute;m'; 
+$MOD_CAPTCHA_CONTROL['TTF_IMAGE']         = 'Obr&aacute;zok s r&ocirc;znymi fontmy a pozad&iacute;m';
+$MOD_CAPTCHA_CONTROL['OLD_IMAGE']         = 'Star&yacute; sp&ocirc;sob (neodpor&uacute;&#269;a sa)';
+$MOD_CAPTCHA_CONTROL['TEXT']              = 'Text-CAPTCHA';
+$MOD_CAPTCHA_CONTROL['CAPTCHA_ENTER_TEXT']= 'Ot&aacute;zky a odpovede';
+$MOD_CAPTCHA_CONTROL['CAPTCHA_TEXT_DESC'] = 'Delete this all to add your own entries'."\n".'or your changes won\'t be saved!'."\n".'### example ###'."\n".'Here you can enter Questions and Answers.'."\n".'Use:'."\n".'?What\'s <b>Claudia</b> Schiffer\'s <b>first name</b>?'."\n".'!Claudia'."\n".'?Question 2'."\n".'!Answer 2'."\n".''."\n".'if language doesn\'t matter.'."\n".' ... '."\n".'Or, if language do matter, use:'."\n".'?EN:What\'s <b>Claudia</b> Schiffer\'s <b>first name</b>?'."\n".'!Claudia'."\n".'?EN:Question 2'."\n".'!Answer 2'."\n".'?DE:Wie ist der <b>Vorname</b> von <b>Claudia</b> Schiffer?'."\n".'!Claudia'."\n".' ... '."\n".'### example ###'."\n".'';
+
+$MOD_CAPTCHA['VERIFICATION']           = 'Overenie';
+$MOD_CAPTCHA['ADDITION']               = 'plus';
+$MOD_CAPTCHA['SUBTRAKTION']            = 'm&iacute;nus';
+$MOD_CAPTCHA['MULTIPLIKATION']         = 'n&aacute;sobi&#357;';
+$MOD_CAPTCHA['VERIFICATION_INFO_RES']  = 'Vlo&#382;te v&yacute;sledok';
+$MOD_CAPTCHA['VERIFICATION_INFO_TEXT'] = 'Vlo&#382;te text';
+$MOD_CAPTCHA['VERIFICATION_INFO_QUEST'] = 'Odpoveï na ot&aacute;zku';
+$MOD_CAPTCHA['INCORRECT_VERIFICATION'] = 'Overenie zlyhalo';
+
+?> 
