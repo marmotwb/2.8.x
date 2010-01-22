2a0d78762c6d4e19f8589d3c0b671cc8a1a11ae2
Luisehahne <Luisehahne@98bf1eab-f5fc-0310-a3ed-aac8851f7404>
2010-01-22 04:05:22 +0000

    Ticket #916 Update backend theme footer

--- branches/2.8.x/wb/templates/wb_theme/jquery/jquery-plugins.js
+++ branches/2.8.x/wb/templates/wb_theme/jquery/jquery-plugins.js
@@ -8,21 +8,17 @@ $(document).ready(function()
         if($(".jsadmin").length) {
             $.insert(WB_URL+"/modules/jsadmin/backend.css");
           }
-/*
-        if($("a[rel^='lightbox']").length) {
-            $.insert(WB_URL+"/include/jquery/plugins/jquery-slimbox2.css");
-            $.insert(WB_URL+"/include/jquery/plugins/jquery-slimbox2-min.js");
-          }
-        if($("textarea.markitup").length) {
-            $.insert(WB_URL+'/modules/markitup/latest/skins/markitup/style.css');
-            $.insert(WB_URL+'/modules/markitup/latest/sets/default/style.css');
-            $.insert(WB_URL+'/modules/markitup/latest/jquery.markitup.pack.js');
-            $.insert(WB_URL+'/modules/markitup/latest/sets/default/set.js');
 
-        	// Add markItUp! to your textarea in one line
-        	// $('textarea.markitup').markItUp( { Settings }, { OptionalExtraSettings } );
-        	$('textarea.markitup').markItUp(mySettings);
+	//Add external link class to external links -
+	$('a[href^="http://"]').filter(function() {
+		//Compare the anchor tag's host name with location's host name
+	    return this.hostname && this.hostname !== location.hostname;
+	  }).addClass("external").attr("target", "_blank");
 
-         }
- */
+	/* Add internal link class to external links -   */
+	$('a[href^="http://"]').filter(function() {
+		//Compare the anchor tag's host name with location's host name
+	    return this.hostname && this.hostname == location.hostname;
+	  }).addClass("internal");
+
 });
