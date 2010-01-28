0cecd20d6084df7ca0fea4dc0172bae3a1af31c1
Luisehahne <Luisehahne@98bf1eab-f5fc-0310-a3ed-aac8851f7404>
2010-01-28 10:59:42 +0000

    Ticket #927 news module language file in swedish update headertext

--- branches/2.8.x/wb/modules/news/languages/NL.php
+++ branches/2.8.x/wb/modules/news/languages/NL.php
@@ -1,32 +1,4 @@
 <?php
-/*
-*
-*                       About WebsiteBaker
-*
-* Website Baker is a PHP-based Content Management System (CMS)
-* designed with one goal in mind: to enable its users to produce websites
-* with ease.
-*
-*                       LICENSE INFORMATION
-*
-* WebsiteBaker is free software; you can redistribute it and/or
-* modify it under the terms of the GNU General Public License
-* as published by the Free Software Foundation; either version 2
-* of the License, or (at your option) any later version.
-*
-* WebsiteBaker is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-* See the GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program; if not, write to the Free Software
-* Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-*
-*                   WebsiteBaker Extra Information
-*
-*
-*/
 /**
  *
  * @category        modules
@@ -62,7 +34,7 @@ $MOD_NEWS['TEXT_COMMENTS'] = 'Commentaren';
 $MOD_NEWS['TEXT_COMMENT'] = 'Commentaar';
 $MOD_NEWS['TEXT_ADD_COMMENT'] = 'Toevoegen commentaar';
 $MOD_NEWS['TEXT_BY'] = 'door';
-$MOD_NEWS['PAGE_NOT_FOUND'] = 'Page not found';
+$MOD_NEWS['PAGE_NOT_FOUND'] = 'Pagina niet gevonden';
 $TEXT['UNKNOWN'] = 'Guest';
 
 ?>
