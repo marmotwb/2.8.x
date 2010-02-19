db56588b1e49a957446cc01637526368ab97f565
Luisehahne <Luisehahne@98bf1eab-f5fc-0310-a3ed-aac8851f7404>
2010-02-19 02:20:58 +0000

    change show_menu2 to correct modul version 4.9

--- branches/2.8.x/wb/modules/show_menu2/info.php
+++ branches/2.8.x/wb/modules/show_menu2/info.php
@@ -1,40 +1,20 @@
 <?php
-
-// $Id$
-
-/*
-    show_menu2: show_menu replacement for Website Baker 
-    Copyright (C) 2006-2009, Brodie Thiesfield
-
-    This program is free software; you can redistribute it and/or
-    modify it under the terms of the GNU General Public License
-    as published by the Free Software Foundation; either version 2
-    of the License, or (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  
-    02110-1301, USA.
-    
-    THANKS
-    ======
-    Thanks to the following people:
-
-        Stuart Fraser
-          * Original request for a better menu system
-
-        Ezra Peisach
-          * Bug fixes
-
-        Thorn
-          * Added support for WB 2.7 multiple groups and publish by date
-    
-*/
+/**
+ *
+ * @category        module
+ * @package         show_menu2
+ * @author          WebsiteBaker Project
+ * @copyright       2004-2009, Ryan Djurovich
+ * @copyright       2009-2010, Website Baker Org. e.V.
+ * @link			http://www.websitebaker2.org/
+ * @license         http://www.gnu.org/licenses/gpl.html
+ * @platform        WebsiteBaker 2.7.0 | 2.8.x
+ * @requirements    PHP 4.4.9 and higher
+ * @version         $Id$
+ * @filesource		$HeadURL$
+ * @lastmodified    $Date$
+ *
+ */
 
 $module_directory = 'show_menu2';
 $module_name = 'show_menu2';
