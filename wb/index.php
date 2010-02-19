ca5a63368cb43a983ca048effdf633aa7fd66b78
Luisehahne <Luisehahne@98bf1eab-f5fc-0310-a3ed-aac8851f7404>
2010-02-19 02:17:53 +0000

    insert $wb->preprocess($output), preprocess no longer needed in modules little modifaction in upgrade-script.php in function check_wb_tables updated module wysiwyg, set to module version 2.8

--- wb/index.php
+++ wb/index.php
@@ -1,37 +1,45 @@
 <?php
-
-// $Id$
-
-/*
-
- Website Baker Project <http://www.websitebaker.org/>
- Copyright (C) 2004-2009, Ryan Djurovich
-
- Website Baker is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 2 of the License, or
- (at your option) any later version.
-
- Website Baker is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with Website Baker; if not, write to the Free Software
- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
-*/
+/**
+ *
+ * @category        frontend
+ * @package         page
+ * @author          WebsiteBaker Project
+ * @copyright       2004-2009, Ryan Djurovich
+ * @copyright       2009-2010, Website Baker Org. e.V.
+ * @link			http://www.websitebaker2.org/
+ * @license         http://www.gnu.org/licenses/gpl.html
+ * @platform        WebsiteBaker 2.8.x
+ * @requirements    PHP 4.4.9 and higher
+ * @version         $Id$
+ * @filesource		$HeadURL$
+ * @lastmodified    $Date$
+ *
+ */
 
 $starttime = array_sum(explode(" ",microtime()));
 
+define('DEBUG', false);
 // Include config file
-require_once(dirname(__FILE__).'/config.php');
+$config_file = dirname(__FILE__).'/config.php';
+if(file_exists($config_file))
+{
+	require_once($config_file);
+}
 
 // Check if the config file has been set-up
-if(!defined('WB_PATH')) {
-	header("Location: install/index.php");
-	exit(0);
+if(!defined('WB_PATH'))
+{
+/*
+ * Anmerkung:  HTTP/1.1 verlangt einen absoluten URI inklusive dem Schema,
+ * Hostnamen und absoluten Pfad als Argument von Location:, manche aber nicht alle
+ * Clients akzeptieren jedoch auch relative URIs.
+ */
+	$host       = $_SERVER['HTTP_HOST'];
+	$uri        = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+	$file       = 'install/index.php';
+	$target_url = 'http://'.$host.$uri.'/'.$file;
+	header('Location: '.$target_url);
+	exit;	// make sure that subsequent code will not be executed
 }
 
 require_once(WB_PATH.'/framework/class.frontend.php');
@@ -55,57 +63,86 @@ require(WB_PATH.'/framework/frontend.functions.php');
 
 // redirect menu-link
 $this_page_id = PAGE_ID;
-if(version_compare(phpversion(), '4.3', '>=')) $php43 = TRUE; else $php43 = FALSE;
-$query_this_module = $database->query("SELECT module, block FROM ".TABLE_PREFIX."sections WHERE page_id = '$this_page_id' AND module = 'menu_link'");
-if($query_this_module->numRows() == 1) { // This is a menu_link. Get link of target-page and redirect
+
+$php43 = version_compare(phpversion(), '4.3', '>=');
+
+$sql  = 'SELECT `module`, `block` FROM `'.TABLE_PREFIX.'sections` ';
+$sql .= 'WHERE `page_id` = '.(int)$this_page_id.' AND `module` = "menu_link"';
+$query_this_module = $database->query($sql);
+if($query_this_module->numRows() == 1)  // This is a menu_link. Get link of target-page and redirect
+{
 	// get target_page_id
-	$table = TABLE_PREFIX.'mod_menu_link';
-	$query_tpid = $database->query("SELECT * FROM $table WHERE page_id = '$this_page_id'");
-	if($query_tpid->numRows() == 1) {
-		$res=$query_tpid->fetchRow();
+	$sql  = 'SELECT * FROM `'.TABLE_PREFIX.'mod_menu_link` WHERE `page_id` = '.(int)$this_page_id;
+	$query_tpid = $database->query($sql);
+	if($query_tpid->numRows() == 1)
+	{
+		$res = $query_tpid->fetchRow();
 		$target_page_id = $res['target_page_id'];
-		$r_type = $res['redirect_type'];
-		$anchor = $res['anchor'];
+		$redirect_type = $res['redirect_type'];
+		$anchor = ($res['anchor'] != '0' ? '#'.(string)$res['anchor'] : '');
 		$extern = $res['extern'];
-		if($anchor != '0') $anchor = ''.$anchor;
-		else $anchor = FALSE;
 		// set redirect-type
-		if($r_type=='301') {
-			if($php43) @header('HTTP/1.1 301 Moved Permanently', TRUE, 301);
-			else @header('HTTP/1.1 301 Moved Permanently');
-		}
-		if($target_page_id == -1) {
-			if($extern!='') {
-				header("Location: $extern".($anchor?'#'.$anchor:''));
+		if($redirect_type == 301)
+		{
+			if($php43)
+			{
+				@header('HTTP/1.1 301 Moved Permanently', TRUE, 301);
 			}
-		} else {
+			else
+			{
+				@header('HTTP/1.1 301 Moved Permanently');
+			}
+		}
+		if($target_page_id == -1)
+		{
+			if($extern != '')
+			{
+				$target_url = $extern.$anchor;
+				header('Location: '.$target_url);
+				exit;
+			}
+		}
+		else
+		{
 			// get link of target-page
-			$table = TABLE_PREFIX.'pages';
-			$query_link = $database->query("SELECT link FROM $table WHERE page_id = '$target_page_id'");
-			if($query_link->numRows() == 1) {
-				$res=$query_link->fetchRow();
-				$target_page_link = $res['link'];
-				header('Location: '.WB_URL.PAGES_DIRECTORY.$target_page_link.PAGE_EXTENSION.($anchor?'#'.$anchor:''));
+			$sql  = 'SELECT `link` FROM `'.TABLE_PREFIX.'pages` WHERE `page_id` = '.$target_page_id;
+			$target_page_link = $database->get_one($sql);
+			if($target_page_link != null)
+			{
+				$target_url = WB_URL.PAGES_DIRECTORY.$target_page_link.PAGE_EXTENSION.$anchor;
+				header('Location: '.$target_url);
 				exit;
 			}
 		}
 	}
 }
-
-// Backwards compatible Frontend filter support
-// include the output filter module routines
-if(file_exists(WB_PATH .'/modules/output_filter/filter-routines.php')) {@require_once(WB_PATH .'/modules/output_filter/filter-routines.php');}
-// Load Droplet engine
-if(file_exists(WB_PATH .'/modules/droplets/droplets.php')) { @require_once(WB_PATH .'/modules/droplets/droplets.php'); }
-
 //Get pagecontent in buffer for Droplets and/or Filter operations
 ob_start();
 require(WB_PATH.'/templates/'.TEMPLATE.'/index.php');
 $output = ob_get_contents();
-if (ob_get_length() > 0) ob_end_clean();
+if(ob_get_length() > 0) { ob_end_clean(); }
 
-if(function_exists('evalDroplets')) { $output = evalDroplets($output); }
-if(function_exists('filter_frontend_output')) { $output = filter_frontend_output($output); }
+// wb->preprocess() -- replace all [wblink123] with real, internal links
+$wb->preprocess($output);
+// Load Droplet engine and process
+if(file_exists(WB_PATH .'/modules/droplets/droplets.php'))
+{
+    include_once(WB_PATH .'/modules/droplets/droplets.php');
+    if(function_exists('evalDroplets'))
+    {
+		$output = evalDroplets($output);
+    }
+}
+// Load backwards compatible frontend filter support and process
+if(file_exists(WB_PATH .'/modules/output_filter/filter-routines.php'))
+{
+    include_once(WB_PATH .'/modules/output_filter/filter-routines.php');
+    if(function_exists('filter_frontend_output'))
+    {
+        $output = filter_frontend_output($output);
+    }
+}
 echo $output;
-
+// end of wb-script
+exit;
 ?>
