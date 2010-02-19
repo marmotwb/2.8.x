59bb35d914193b757dcf71883cdd291141b1c2af
Luisehahne <Luisehahne@98bf1eab-f5fc-0310-a3ed-aac8851f7404>
2010-02-19 02:28:35 +0000

    update headerinfo change $search_pattern for check if Javascript mailto encryption is enabled module output_filter set to version 0.12

--- branches/2.8.x/wb/modules/output_filter/info.php
+++ branches/2.8.x/wb/modules/output_filter/info.php
@@ -1,42 +1,26 @@
 <?php
+/**
+ *
+ * @category        modules
+ * @package         output_filter
+ * @author          WebsiteBaker Project
+ * @copyright       2004-2009, Ryan Djurovich
+ * @copyright       2009-2010, Website Baker Org. e.V.
+ * @link			http://www.websitebaker2.org/
+ * @license         http://www.gnu.org/licenses/gpl.html
+ * @platform        WebsiteBaker 2.8.x
+ * @requirements    PHP 4.4.9 and higher
+ * @version         $Id$
+ * @filesource		$HeadURL$
+ * @lastmodified    $Date$
+ *
+ */
 
-// $Id$
-
-/*
-
- Website Baker Project <http://www.websitebaker.org/>
- Copyright (C) 2004-2009, Ryan Djurovich
-
- Website Baker is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 2 of the License, or
- (at your option) any later version.
-
- Website Baker is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with Website Baker; if not, write to the Free Software
- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
- -----------------------------------------------------------------------------------------
-  Output Filter tool for Website Baker v2.7
-  Licencsed under GNU, written by Christian Sommer (Doc)
- -----------------------------------------------------------------------------------------
-   v0.11  (Chritian Sommer; 06 Feb, 2008)
-    ~ renamed module to output filter, splitted email filter in two functions (text emails, mailto links)
-
-   v0.10  (Christian Sommer; 21 Jan, 2008)
-    ~ initial module release (Note: requires WB 2.7 core file changes to work)
-
-*/
 
 $module_directory 	= 'output_filter';
 $module_name 				= 'Frontend Output Filter';
 $module_function 		= 'tool';
-$module_version 		= '0.11';
+$module_version 		= '0.12';
 $module_platform 		= '2.7 | 2.8.x';
 $module_author 			= 'Christian Sommer (doc)';
 $module_license 		= 'GNU General Public License';
