0bb7547af51c72908aa9024fb0bf627818350789
Luisehahne <Luisehahne@98bf1eab-f5fc-0310-a3ed-aac8851f7404>
2010-03-19 21:05:49 +0000

    rework admin/pages/save.php so that modules can use it fixed missing global variable in install to work in modules

--- branches/2.8.x/wb/admin/modules/manual_install.php
+++ branches/2.8.x/wb/admin/modules/manual_install.php
@@ -1,43 +1,28 @@
 <?php
 /**
- * $Id$
- * Website Baker Manual module installation
  *
- * This file contains the function to invoke the module install or upgrade
- * scripts update the Add-on information from the
- * database with the ones stored in the Add-on files (e.g. info.php or EN.php)
+ * @category        admin
+ * @package         modules
+ * @author          Christian Sommer
+ * @author          WebsiteBaker Project
+ * @copyright       2004-2009, Ryan Djurovich
+ * @copyright       2009-2010, Website Baker Org. e.V.
+ * @link			http://www.websitebaker2.org/
+ * @license         http://www.gnu.org/licenses/gpl.html
+ * @platform        WebsiteBaker 2.8.x
+ * @requirements    PHP 4.4.9 and higher
+ * @version         $Id$
+ * @filesource		$HeadURL$
+ * @lastmodified    $Date$
  *
- * LICENSE: GNU Lesser General Public License 3.0
- * 
- * @author		Christian Sommer
- * @copyright	(c) 2009
- * @license		http://www.gnu.org/copyleft/lesser.html
- * @version		0.2.0
- * @platform	Website Baker 2.7
- *
- * Website Baker Project <http://www.websitebaker.org/>
- * Copyright (C) 2004-2009, Ryan Djurovich
- *
- * Website Baker is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * Website Baker is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Website Baker; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-*/
+ */
 
 /**
  * check if there is anything to do
  */
-if (!(isset($_POST['action']) && in_array($_POST['action'], array('install', 'upgrade', 'uninstall')))) die(header('Location: index.php?advanced'));
-if (!(isset($_POST['file']) && $_POST['file'] != '' && (strpos($_POST['file'], '..') === false))) die(header('Location: index.php?advanced'));
+
+if (!(isset($_POST['action']) && in_array($_POST['action'], array('install', 'upgrade', 'uninstall')))) { die(header('Location: index.php?advanced')); }
+if (!(isset($_POST['file']) && $_POST['file'] != '' && (strpos($_POST['file'], '..') === false))){  die(header('Location: index.php?advanced'));  }
 
 /**
  * check if user has permissions to access this file
@@ -48,7 +33,7 @@ require_once('../../framework/class.admin.php');
 
 // check user permissions for admintools (redirect users with wrong permissions)
 $admin = new admin('Admintools', 'admintools', false, false);
-if ($admin->get_permission('admintools') == false) die(header('Location: ../../index.php'));
+if ($admin->get_permission('admintools') == false) { die(header('Location: ../../index.php')); }
 
 // check if the referer URL if available
 $referer = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : 
@@ -57,7 +42,7 @@ $referer = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] :
 // if referer is set, check if script was invoked from "admin/modules/index.php"
 $required_url = ADMIN_URL . '/modules/index.php';
 if ($referer != '' && (!(strpos($referer, $required_url) !== false || strpos($referer, $required_url) !== false))) 
-	die(header('Location: ../../index.php'));
+{ die(header('Location: ../../index.php')); }
 
 // include WB functions file
 require_once(WB_PATH . '/framework/functions.php');
@@ -74,7 +59,13 @@ $js_back = ADMIN_URL . '/modules/index.php?advanced';
  */
 // check if specified module folder exists
 $mod_path = WB_PATH . '/modules/' . basename(WB_PATH . '/' . $_POST['file']);
-if (!file_exists($mod_path . '/' . $_POST['action'] . '.php')) $admin->print_error($TEXT['NOT_FOUND'] . ': <tt>"' . htmlentities(basename($mod_path)) . '/' . $_POST['action'] . '.php"</tt> ', $js_back);
+
+// let the old variablename if module use it
+$module_dir = $mod_path;
+if (!file_exists($mod_path . '/' . $_POST['action'] . '.php'))
+{
+    $admin->print_error($TEXT['NOT_FOUND'].': <tt>"'.htmlentities(basename($mod_path)).'/'.$_POST['action'].'.php"</tt> ', $js_back);
+}
 
 // include modules install.php script
 require($mod_path . '/' . $_POST['action'] . '.php');
@@ -83,7 +74,8 @@ require($mod_path . '/' . $_POST['action'] . '.php');
 load_module($mod_path, false);
 $msg = $TEXT['EXECUTE'] . ': <tt>"' . htmlentities(basename($mod_path)) . '/' . $_POST['action'] . '.php"</tt>';
 
-switch ($_POST['action']) {
+switch ($_POST['action'])
+{
 	case 'install':
 		$admin->print_success($msg, $js_back);
 		break;
