251870c311b85d436c28b3e8889f050ca1e3d597
Luisehahne <Luisehahne@98bf1eab-f5fc-0310-a3ed-aac8851f7404>
2010-04-01 00:43:23 +0000

    Ticket 963 Minor XSS issue in admin login update jquery-ui-min.js from 1.7.2 to 1.8.0 Fixed typo in the english language file

--- branches/2.8.x/wb/include/jquery/jquery_theme.js
+++ branches/2.8.x/wb/include/jquery/jquery_theme.js
@@ -2,5 +2,5 @@ var JQUERY_THEME = WB_URL+'/include/jquery';
 
 $(document).ready(function() {
         $.insert(JQUERY_THEME+'/jquery-ui.css');
-      /*   $.insert(JQUERY_THEME+'/ui.theme.css');  */
+        $.insert(JQUERY_THEME+'/jquery-ui-min.js');
 });
