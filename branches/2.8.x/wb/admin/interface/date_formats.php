8d7ff97cdbf037e13ca62b54ec8454d0746f4df1
Luisehahne <Luisehahne@98bf1eab-f5fc-0310-a3ed-aac8851f7404>
2010-04-15 14:07:39 +0000

    Update headerinfos admin/interface

--- branches/2.8.x/wb/admin/interface/date_formats.php
+++ branches/2.8.x/wb/admin/interface/date_formats.php
@@ -1,35 +1,23 @@
 <?php
-
-// $Id$
-
-/*
-
- Website Baker Project <http://www.websitebaker.org/>
- Copyright (C) 2004-2009, Ryan Djurovich
-
- Website Baker is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 2 of the License, or
- (at your option) any later version.
-
- Website Baker is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with Website Baker; if not, write to the Free Software
- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
-*/
-
-/*
-
-Date format list file
-
-This file is used to generate a list of date formats for the user to select
-
-*/
+/**
+ *
+ * @category        admin
+ * @package         interface
+ * @author          WebsiteBaker Project
+ * @copyright       2004-2009, Ryan Djurovich
+ * @copyright       2009-2010, Website Baker Org. e.V.
+ * @link            http://www.websitebaker2.org/
+ * @license         http://www.gnu.org/licenses/gpl.html
+ * @platform        WebsiteBaker 2.8.x
+ * @requirements    PHP 4.4.9 and higher
+ * @version         $Id$
+ * @filesource      $HeadURL$
+ * @lastmodified    $Date$
+ *
+ * Date format list file
+ * This file is used to generate a list of date formats for the user to select
+ *
+ */
 
 if(!defined('WB_URL')) {
 	header('Location: ../../../index.php');
@@ -45,7 +33,7 @@ if(!defined('DATE_FORMATS_LOADED')) {
 $DATE_FORMATS = array();
 
 // Get the current time (in the users timezone if required)
-$actual_time = time()+ ((isset($user_time) AND $user_time == true) ? TIMEZONE : DEFAULT_TIMEZONE);
+$actual_time = time()+ ((isset($user_time) && $user_time == true) ? TIMEZONE : DEFAULT_TIMEZONE);
 
 // Add values to list
 $DATE_FORMATS['l,|jS|F,|Y'] = gmdate('l, jS F, Y', $actual_time);
@@ -61,8 +49,10 @@ $DATE_FORMATS['d/m/Y'] = gmdate('d/m/Y', $actual_time).' (D/M/Y)';
 $DATE_FORMATS['m/d/Y'] = gmdate('m/d/Y', $actual_time).' (M/D/Y)';
 
 // Add "System Default" to list (if we need to)
-if(isset($user_time) AND $user_time == true) {
-	if(isset($TEXT['SYSTEM_DEFAULT'])) {
+if(isset($user_time) && $user_time == true)
+{
+	if(isset($TEXT['SYSTEM_DEFAULT']))
+	{
 		$DATE_FORMATS['system_default'] = gmdate(DEFAULT_DATE_FORMAT, $actual_time).' ('.$TEXT['SYSTEM_DEFAULT'].')';
 	} else {
 		$DATE_FORMATS['system_default'] = gmdate(DEFAULT_DATE_FORMAT, $actual_time).' (System Default)';
--- branches/2.8.x/wb/admin/interface/timezones.php
+++ branches/2.8.x/wb/admin/interface/timezones.php
@@ -1,35 +1,23 @@
 <?php
-
-// $Id$
-
-/*
-
- Website Baker Project <http://www.websitebaker.org/>
- Copyright (C) 2004-2009, Ryan Djurovich
-
- Website Baker is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 2 of the License, or
- (at your option) any later version.
-
- Website Baker is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with Website Baker; if not, write to the Free Software
- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
-*/
-
-/*
-
-Timezone list file
-
-This file is used to generate a list of timezones for the user to select
-
-*/
+/**
+ *
+ * @category        admin
+ * @package         interface
+ * @author          WebsiteBaker Project
+ * @copyright       2004-2009, Ryan Djurovich
+ * @copyright       2009-2010, Website Baker Org. e.V.
+ * @link            http://www.websitebaker2.org/
+ * @license         http://www.gnu.org/licenses/gpl.html
+ * @platform        WebsiteBaker 2.8.x
+ * @requirements    PHP 4.4.9 and higher
+ * @version         $Id$
+ * @filesource      $HeadURL$
+ * @lastmodified    $Date$
+ *
+ * Timezone list file
+ * This file is used to generate a list of timezones for the user to select
+ *
+ */
 
 if(!defined('WB_URL')) {
 	header('Location: ../index.php');
@@ -38,13 +26,7 @@ if(!defined('WB_URL')) {
 
 // Create array
 $TIMEZONES = array();
-
-// Add "System Default" to top of list
-if(isset($TEXT['SYSTEM_DEFAULT'])) {
-	$TIMEZONES['-20'] = $TEXT['SYSTEM_DEFAULT'];
-} else {
-	$TIMEZONES['-20'] = 'System Default';
-}
+$actual_timezone = ( DEFAULT_TIMEZONE <> 0 ) ? DEFAULT_TIMEZONE/3600 : 0;
 
 $TIMEZONES['-12'] = 'GMT - 12 Hours';
 $TIMEZONES['-11'] = 'GMT -11 Hours';
@@ -78,5 +60,19 @@ $TIMEZONES['10'] = 'GMT +10 Hours';
 $TIMEZONES['11'] = 'GMT +11 Hours';
 $TIMEZONES['12'] = 'GMT +12 Hours';
 $TIMEZONES['13'] = 'GMT +13 Hours';
+
+// Add "System Default" to list (if we need to)
+if(isset($user_time) && $user_time == true)
+{
+	if(isset($TEXT['SYSTEM_DEFAULT']))
+	{
+		$TIMEZONES['system_default'] = $TIMEZONES[$actual_timezone].' ('.$TEXT['SYSTEM_DEFAULT'].')';
+	} else {
+		$TIMEZONES['system_default'] = $TIMEZONES[$actual_timezone].' (System Default)';
+	}
+}
+
+// Reverse array so "System Default" is at the top
+$TIMEZONES = array_reverse($TIMEZONES, true);
 
 ?>
