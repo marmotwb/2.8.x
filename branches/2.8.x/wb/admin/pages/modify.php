7115de9fee9b11b8122441261bb5deb342495997
Luisehahne <Luisehahne@98bf1eab-f5fc-0310-a3ed-aac8851f7404>
2010-04-22 03:13:59 +0000

    start recoding without style display:none

--- branches/2.8.x/wb/admin/pages/modify.php
+++ branches/2.8.x/wb/admin/pages/modify.php
@@ -17,7 +17,7 @@
 */
 
 // Get page id
-if(!isset($_GET['page_id']) OR !is_numeric($_GET['page_id'])) {
+if(!isset($_GET['page_id']) || !is_numeric($_GET['page_id'])) {
 	header("Location: index.php");
 	exit(0);
 } else {
@@ -27,6 +27,7 @@ if(!isset($_GET['page_id']) OR !is_numeric($_GET['page_id'])) {
 // Create new admin object
 require('../../config.php');
 require_once(WB_PATH.'/framework/class.admin.php');
+
 $admin = new admin('Pages', 'pages_modify');
 
 // Get perms
@@ -52,43 +53,64 @@ $modified_ts = ($results_array['modified_when'] != 0)
 $template = new Template(THEME_PATH.'/templates');
 $template->set_file('page', 'pages_modify.htt');
 $template->set_block('page', 'main_block', 'main');
+
 $template->set_var(array(
-								'PAGE_ID' => $results_array['page_id'],
-								'PAGE_TITLE' => ($results_array['page_title']),
-								'MENU_TITLE' => ($results_array['menu_title']),
-								'MODIFIED_BY' => $user['display_name'],
-								'MODIFIED_BY_USERNAME' => $user['username'],
-								'MODIFIED_WHEN' => $modified_ts,
-								'ADMIN_URL' => ADMIN_URL,
-								'WB_URL' => WB_URL,
-								'WB_PATH' => WB_PATH,
-								'THEME_URL' => THEME_URL
-								)
-						);
-if($modified_ts == 'Unknown') {
+			'PAGE_ID' => $results_array['page_id'],
+			'PAGE_TITLE' => ($results_array['page_title']),
+			'MENU_TITLE' => ($results_array['menu_title']),
+			'ADMIN_URL' => ADMIN_URL,
+			'WB_URL' => WB_URL,
+			'WB_PATH' => WB_PATH,
+			'THEME_URL' => THEME_URL
+			));
+
+$template->set_var(array(
+			'MODIFIED_BY' => $user['display_name'],
+			'MODIFIED_BY_USERNAME' => $user['username'],
+			'MODIFIED_WHEN' => $modified_ts,
+			'LAST_MODIFIED' => $MESSAGE['PAGES']['LAST_MODIFIED'],
+			));
+
+$template->set_block('main_block', 'show_modify_block', 'show_modify');
+if($modified_ts == 'Unknown')
+{
+    $template->set_block('show_modify', '');
 	$template->set_var('CLASS_DISPLAY_MODIFIED', 'hide');
+
 } else {
 	$template->set_var('CLASS_DISPLAY_MODIFIED', '');
+    $template->parse('show_modify', 'show_modify_block', true);
 }
 
 // Work-out if we should show the "manage sections" link
-$query_sections = $database->query("SELECT section_id FROM ".TABLE_PREFIX."sections WHERE page_id = '$page_id' AND module = 'menu_link'");
-if($query_sections->numRows() > 0) {
+$sql  = 'SELECT `section_id` FROM `'.TABLE_PREFIX.'sections` WHERE `page_id` = '.(int)$page_id.' ';
+$sql .= 'AND `module` = "menu_link"';
+$query_sections = $database->query($sql);
+
+$template->set_block('main_block', 'show_section_block', 'show_section');
+if($query_sections->numRows() > 0)
+{
+	$template->set_block('show_section', '');
 	$template->set_var('DISPLAY_MANAGE_SECTIONS', 'display:none;');
-} elseif(MANAGE_SECTIONS == 'enabled') {
+
+} elseif(MANAGE_SECTIONS == 'enabled')
+{
+
 	$template->set_var('TEXT_MANAGE_SECTIONS', $HEADING['MANAGE_SECTIONS']);
+    $template->parse('show_section', 'show_section_block', true);
+
 } else {
+	$template->set_block('show_section', '');
 	$template->set_var('DISPLAY_MANAGE_SECTIONS', 'display:none;');
+
 }
 
 // Insert language TEXT
 $template->set_var(array(
-								'TEXT_CURRENT_PAGE' => $TEXT['CURRENT_PAGE'],
-								'TEXT_CHANGE_SETTINGS' => $TEXT['CHANGE_SETTINGS'],
-								'LAST_MODIFIED' => $MESSAGE['PAGES']['LAST_MODIFIED'],
-								'HEADING_MODIFY_PAGE' => $HEADING['MODIFY_PAGE']
-								)
-						);
+				'TEXT_CURRENT_PAGE' => $TEXT['CURRENT_PAGE'],
+				'TEXT_CHANGE_SETTINGS' => $TEXT['CHANGE_SETTINGS'],
+				'HEADING_MODIFY_PAGE' => $HEADING['MODIFY_PAGE']
+				));
 
 // Parse and print header template
 $template->parse('main', 'main_block', false);
@@ -103,7 +125,8 @@ if (SECTION_BLOCKS)
 		$row = $result->fetchRow();
 		$page_template = ($row['template'] == '') ? DEFAULT_TEMPLATE : $row['template'];
 		// include template info file if exists
-		if (file_exists(WB_PATH . '/templates/' . $page_template . '/info.php')) {
+		if (file_exists(WB_PATH . '/templates/' . $page_template . '/info.php'))
+		{
 			include_once(WB_PATH . '/templates/' . $page_template . '/info.php');
 		}
 	}
@@ -113,17 +136,13 @@ if (SECTION_BLOCKS)
 $module_permissions = $_SESSION['MODULE_PERMISSIONS'];
 // workout for edit only one section for faster pageloading
 // Constant later set in wb_settings, in meantime defined in framework/initialize.php
-if(defined('EDIT_ONE_SECTION') and EDIT_ONE_SECTION and is_numeric($sectionId))
-{
-$query_sections = $database->query("SELECT section_id, module, block
-	FROM ".TABLE_PREFIX."sections WHERE section_id = '$sectionId' ORDER BY position ASC");
-}
-else
-{
-$query_sections = $database->query("SELECT section_id, module, block
-	FROM ".TABLE_PREFIX."sections WHERE page_id = '$page_id' ORDER BY position ASC");
-}
+$sql = 'SELECT `section_id`, `module`, `block` FROM `'.TABLE_PREFIX.'sections` ';
 
+$sql .= (defined('EDIT_ONE_SECTION') && EDIT_ONE_SECTION && is_numeric($sectionId))
+		? 'WHERE `section_id` = '.$sectionId
+		: 'WHERE `page_id` = '.intval($page_id);
+$sql .= ' ORDER BY position ASC';
+$query_sections = $database->query($sql);
 if($query_sections->numRows() > 0)
 {
 	while($section = $query_sections->fetchRow())
