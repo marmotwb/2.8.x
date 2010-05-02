77da7dbcd0c2cabf28c13cbd4d0c43f6d83bb7c1
Luisehahne <Luisehahne@98bf1eab-f5fc-0310-a3ed-aac8851f7404>
2010-05-02 11:17:20 +0000

    added IdKey to class secureForm

--- wb/framework/class.secureform.php
+++ wb/framework/class.secureform.php
@@ -23,14 +23,21 @@ class SecureForm {
 
 	/* insert global vars here... */
 
-	var $_FTAN  = '';
-	var $_IDKEYs = array();
+	var $_FTAN   = '';
+	var $_IDKEYs = '';
+	var $_salt   = '';
 
 	function SecureForm()
 	{
 //		$this->__construct();
 		$this->_FTAN  = '';
-// 		if(isset($_SESSION['FTAN'])) { unset($_SESSION['FTAN']); }
+		$this->_salt = $this->_generate_salt();
+		if(isset($_SESSION['IDKEYS']))
+		{
+			$this->_IDKEYs = $_SESSION['IDKEYS'];
+		}else {
+			$this->_IDKEYs = array();
+		}
 	}
 //	function __construct()
 //	{
@@ -38,6 +45,26 @@ class SecureForm {
 //		if(isset($_SESSION['FTAN'])) { unset($_SESSION['FTAN']); }
 //	}
 
+
+	function _generate_salt()
+	{
+		// server depending values
+ 		$salt  = ( isset($_SERVER['SERVER_SIGNATURE']) ) ? $_SERVER['SERVER_SIGNATURE'] : '2';
+		$salt .= ( isset($_SERVER['SERVER_SOFTWARE']) ) ? $_SERVER['SERVER_SOFTWARE'] : '3';
+		$salt .= ( isset($_SERVER['SERVER_NAME']) ) ? $_SERVER['SERVER_NAME'] : '5';
+		$salt .= ( isset($_SERVER['SERVER_ADDR']) ) ? $_SERVER['SERVER_ADDR'] : '7';
+		$salt .= ( isset($_SERVER['SERVER_PORT']) ) ? $_SERVER['SERVER_PORT'] : '11';
+		$salt .= ( isset($_SERVER['SERVER_ADMIN']) ) ? $_SERVER['SERVER_ADMIN'] : '13';
+		$salt .= PHP_VERSION;
+		// client depending values
+		$salt .= ( isset($_SERVER['HTTP_ACCEPT']) ) ? $_SERVER['HTTP_ACCEPT'] : '17';
+		$salt .= ( isset($_SERVER['HTTP_ACCEPT_CHARSET']) ) ? $_SERVER['HTTP_ACCEPT_CHARSET'] : '19';
+		$salt .= ( isset($_SERVER['HTTP_ACCEPT_ENCODING']) ) ? $_SERVER['HTTP_ACCEPT_ENCODING'] : '23';
+		$salt .= ( isset($_SERVER['HTTP_ACCEPT_LANGUAGE']) ) ? $_SERVER['HTTP_ACCEPT_LANGUAGE'] : '29';
+		$salt .= ( isset($_SERVER['HTTP_CONNECTION']) ) ? $_SERVER['HTTP_CONNECTION'] : '31';
+		$salt .= ( isset($_SERVER['HTTP_USER_AGENT']) ) ? $_SERVER['HTTP_USER_AGENT'] : '37';
+		return $salt;
+	}
 /*
  * creates Formular transactionnumbers for unique use
  * @access public
@@ -58,16 +85,9 @@ class SecureForm {
 			}else{
 				$time = (string)time();
 			}
-			$salt  = ( isset($_SERVER['HTTP_ACCEPT']) ? $_SERVER['HTTP_ACCEPT'] : '');
-			$salt .= ( isset($_SERVER['HTTP_ACCEPT_CHARSET']) ? $_SERVER['HTTP_ACCEPT_CHARSET'] : '');
-			$salt .= ( isset($_SERVER['HTTP_ACCEPT_ENCODING']) ? $_SERVER['HTTP_ACCEPT_ENCODING'] : '');
-			$salt .= ( isset($_SERVER['HTTP_ACCEPT_LANGUAGE']) ? $_SERVER['HTTP_ACCEPT_LANGUAGE'] : '');
-			$salt .= ( isset($_SERVER['HTTP_CONNECTION']) ? $_SERVER['HTTP_CONNECTION'] : '');
-			$salt .= ( isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : '');
-			$salt .= ( isset($_SERVER['SERVER_ADDR']) ? $_SERVER['SERVER_ADDR'] : '');
-			$salt  = ( $salt !== '' ) ? $salt : 'eXtremelyHotTomatoJuice';
-			$this->_FTAN = md5($time.$salt);
+			$this->_FTAN = md5($time.$this->_salt);
 			$_SESSION['FTAN'] = $this->_FTAN;
+
 		}
 		$ftan0 = 'a'.substr($this->_FTAN, -(10 + hexdec(substr($this->_FTAN, 1))), 10);
 		$ftan1 = 'a'.substr($this->_FTAN, hexdec(substr($this->_FTAN, -1)), 10);
@@ -109,8 +129,56 @@ class SecureForm {
 		return $retval;
 	}
 
+/*
+ * save values in session and returns a ID-key
+ * @access public
+ * @param mixed $value: the value for witch a key shall generated and memorized
+ * @return string:      a MD5-Key to use instead of the real value
+ *
+ * requirements: an active session must be available
+ */
+	function getIDKEY($value)
+	{
+		$isarray = is_array($value);
+		if( $isarray ) { $value = serialize($value); }
+		$key = md5($this->_salt.(string)$value);
+		if( $isarray ) { $key[5] = 'h'; }
+		$added = false;
+		while(!$added)
+		{
+			if( !array_key_exists($key, $this->_IDKEYs) )
+			{
+				$this->_IDKEYs[$key] = $value;
+				$added = true;
+			}else {
+			// if key already exist, increment the last four digits until the key is unique
+				$key = substr($key, -4).dechex(('0x'.substr($key0, -4)) + 1);
+			}
+		}
+		$_SESSION['IDKEYS'] = $this->_IDKEYs;
+		return $key;
+	}
 
-
+/*
+ * search for key in session and returns the original value
+ * @access public
+ * @param string $key: the alias-key from the original value
+ * @return mixed: the original value (string, numeric, array) or NULL if request fails
+ *
+ * requirements: an active session must be available
+ */
+	function checkIDKEY( $key )
+	{
+		$value = null;
+		if( array_key_exists($key, $this->_IDKEYs))
+		{
+			$value = $this->_IDKEYs[$key];
+			unset($this->_IDKEYs[$key]);
+			$_SESSION['IDKEYS'] = $this->_IDKEYs;
+			if($value[5] == 'h') { $value = unserialize($value); }
+		}
+		return $value;
+	}
     //put your code here
 }
 ?>
