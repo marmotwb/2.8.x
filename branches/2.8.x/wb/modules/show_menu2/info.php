41ea67eb2c2aac6397f540c82aa56a3b9025a67f
Luisehahne <Luisehahne@98bf1eab-f5fc-0310-a3ed-aac8851f7404>
2011-01-10 12:14:10 +0000

    update headerinfos

--- branches/2.8.x/wb/modules/show_menu2/info.php
+++ branches/2.8.x/wb/modules/show_menu2/info.php
@@ -5,11 +5,11 @@
  * @package         show_menu2
  * @author          WebsiteBaker Project
  * @copyright       2004-2009, Ryan Djurovich
- * @copyright       2009-2010, Website Baker Org. e.V.
+ * @copyright       2009-2011, Website Baker Org. e.V.
  * @link			http://www.websitebaker2.org/
  * @license         http://www.gnu.org/licenses/gpl.html
  * @platform        WebsiteBaker 2.7.0
- * @requirements    PHP 4.4.9 and higher
+ * @requirements    PHP 5.2.2 and higher
  * @version         $Id$
  * @filesource		$HeadURL$
  * @lastmodified    $Date$
