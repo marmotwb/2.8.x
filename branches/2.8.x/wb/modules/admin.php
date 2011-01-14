7de68e6976a962730a3a67e8af48ece31d407b19
Luisehahne <Luisehahne@98bf1eab-f5fc-0310-a3ed-aac8851f7404>
2011-01-14 20:55:13 +0000

    fix known error in drag and drop

--- branches/2.8.x/wb/modules/admin.php
+++ branches/2.8.x/wb/modules/admin.php
@@ -8,7 +8,7 @@
  * @copyright       2009-2011, Website Baker Org. e.V.
  * @link			http://www.websitebaker2.org/
  * @license         http://www.gnu.org/licenses/gpl.html
- * @platform        WebsiteBaker 2.8.x
+ * @platform        WebsiteBaker 2.9.x
  * @requirements    PHP 5.2.2 and higher
  * @version         $Id$
  * @filesource		$HeadURL$
@@ -25,10 +25,10 @@ if(!defined('WB_URL')) {
 // Get page id
 if(isset($_GET['page_id']) && is_numeric($_GET['page_id']))
 {
-	$page_id = $_GET['page_id'];
+	$page_id = (int)$_GET['page_id'];
 } elseif(isset($_POST['page_id']) && is_numeric($_POST['page_id']))
 {
-	$page_id = $_POST['page_id'];
+	$page_id = (int)$_POST['page_id'];
 } else {
 	header("Location: index.php");
 	exit(0);
@@ -37,13 +37,13 @@ if(isset($_GET['page_id']) && is_numeric($_GET['page_id']))
 // Get section id if there is one
 if(isset($_GET['section_id']) && is_numeric($_GET['section_id']))
 {
-	$section_id = $_GET['section_id'];
+	$section_id = (int)$_GET['section_id'];
 } elseif(isset($_POST['section_id']) && is_numeric($_POST['section_id']))
 {
-	$section_id = $_POST['section_id'];
+	$section_id = (int)$_POST['section_id'];
 } else {
 	// Check if we should redirect the user if there is no section id
-	if (isset($no_section_required))
+	if(!isset($section_required))
 	{
 		$section_id = 0;
 	} else {
@@ -61,48 +61,41 @@ include(WB_PATH.'/framework/class.admin.php');
 $admin = new admin('Pages', 'pages_modify');
 
 // Get perms
-// $database = new database();
 $sql  = 'SELECT `admin_groups`,`admin_users` FROM `'.TABLE_PREFIX.'pages` ';
 $sql .= 'WHERE `page_id` = '.intval($page_id);
-
-$res_pages = $database->query($sql);
-$rec_pages = $res_pages->fetchRow();
-
-$old_admin_groups = explode(',', str_replace('_', '', $rec_pages['admin_groups']));
-$old_admin_users  = explode(',', str_replace('_', '', $rec_pages['admin_users']));
-
-$in_group = FALSE;
-foreach($admin->get_groups_id() as $cur_gid)
+$ok = false;
+if( ($res_pages = $database->query($sql)) )
 {
-    if (in_array($cur_gid, $old_admin_groups))
+	if( ($rec_pages = $res_pages->fetchRow()) )
 	{
-        $in_group = TRUE;
-    }
-}
-if((!$in_group) && !is_numeric(array_search($admin->get_user_id(), $old_admin_users)))
-{
-	$admin->print_error($MESSAGE['PAGES']['INSUFFICIENT_PERMISSIONS']);
-}
-
-// Check whether the section_id belongs to the page_id at all
-if ($section_id != 0) {
-	$sql  = "SELECT `position` FROM `".TABLE_PREFIX."sections` WHERE `page_id` = '$page_id' AND `section_id` = '$section_id'";
-	$res_sec = $database->query($sql);
-	if ($database->is_error())
-	{
-		$admin->print_error($database->get_error());
+		if( $admin->ami_group_member($rec_pages['admin_groups']) || $admin->ami_group_member($rec_pages['admin_users']) )
+		{
+			$ok = true;
+		}
 	}
-	if ($res_sec->numRows() == 0)
-	{
-		$admin->print_error($MESSAGE['PAGES']['NOT_FOUND']);
-	} 
 }
+if( !$ok ) { $admin->print_error($MESSAGE['PAGES']['INSUFFICIENT_PERMISSIONS']); }
+
+//$old_admin_groups = explode(',', str_replace('_', '', $rec_pages['admin_groups']));
+//$old_admin_users  = explode(',', str_replace('_', '', $rec_pages['admin_users']));
+//
+//$in_group = FALSE;
+//foreach($admin->get_groups_id() as $cur_gid)
+//{
+//    if (in_array($cur_gid, $old_admin_groups))
+//	{
+//        $in_group = TRUE;
+//    }
+//}
+//if((!$in_group) && !is_numeric(array_search($admin->get_user_id(), $old_admin_users)))
+//{
+//	$admin->print_error($MESSAGE['PAGES']['INSUFFICIENT_PERMISSIONS']);
+//}
 
 // Workout if the developer wants to show the info banner
 if(isset($print_info_banner) && $print_info_banner == true)
 {
 	// Get page details
-	// $database = new database(); not needed
 	$sql  = 'SELECT `page_id`,`page_title`,`modified_by`,`modified_when` FROM `'.TABLE_PREFIX.'pages` ';
 	$sql .= 'WHERE `page_id` = '.intval($page_id);
 	$res_pages = $database->query($sql);
