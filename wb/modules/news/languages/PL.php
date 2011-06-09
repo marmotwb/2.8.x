301ab6acdb33cb8434571f60dad76e7571183f45
Luisehahne <Luisehahne@98bf1eab-f5fc-0310-a3ed-aac8851f7404>
2011-06-09 22:21:43 +0000

    add PL languages to news and captcha_control (tks to vixrealitum) update PL language for WebsiteBaker (tks to vixrealitum)

---
+++ wb/modules/news/languages/PL.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ *
+ * @category        modules
+ * @package         news
+ * @author          WebsiteBaker Project
+ * @copyright       2004-2009, Ryan Djurovich
+ * @copyright       2009-2011, Website Baker Org. e.V.
+ * @link			http://www.websitebaker2.org/
+ * @license         http://www.gnu.org/licenses/gpl.html
+ * @platform        WebsiteBaker 2.8.x
+ * @requirements    PHP 5.2.2 and higher
+ * @version         $Id$
+ * @filesource		$HeadURL$
+ * @lastmodified    $Date$
+ *
+ */
+
+//Modul Description
+$module_description = 'Ten moduł wyświetla stronę Newsów.';
+
+//Variables for the backend
+$MOD_NEWS['SETTINGS'] = 'Ustawienia';
+
+//Variables for the frontend
+$MOD_NEWS['TEXT_READ_MORE'] = 'Czytaj więcej';
+$MOD_NEWS['TEXT_POSTED_BY'] = 'Napisał(a):';
+$MOD_NEWS['TEXT_ON'] = 'dnia';
+$MOD_NEWS['TEXT_LAST_CHANGED'] = 'Edytowano';
+$MOD_NEWS['TEXT_AT'] = 'o';
+$MOD_NEWS['TEXT_BACK'] = 'Wstecz';
+$MOD_NEWS['TEXT_COMMENTS'] = 'Komentarze';
+$MOD_NEWS['TEXT_COMMENT'] = 'Dodaj komentarz';
+$MOD_NEWS['TEXT_ADD_COMMENT'] = 'Dodaj komentarz';
+$MOD_NEWS['TEXT_BY'] = 'Dodano:';
+$MOD_NEWS['PAGE_NOT_FOUND'] = 'Strona nie istnieje';
+$TEXT['UNKNOWN'] = 'Gość';
+
+?>
