9c8fa0e411cc63b51c3f5ad58e07cf16b4c57abd
Luisehahne <Luisehahne@98bf1eab-f5fc-0310-a3ed-aac8851f7404>
2011-06-26 19:28:18 +0000

    Ticket 1099: change title for add_child_page in page tree

--- wb/admin/pages/index.php
+++ wb/admin/pages/index.php
@@ -287,7 +287,7 @@ function make_list($parent = 0, $editable_pages = 0) {
 				?>
 				<td class="list_actions">
 					<?php if(($admin->get_permission('pages_add')) == (true && $can_modify == true) && ($page['visibility'] != 'deleted')) { ?>
-					<a href="javascript:add_child_page('<?php echo $page['page_id']; ?>');" title="<?php echo $HEADING['ADD_PAGE']; ?>">
+					<a href="javascript:add_child_page('<?php echo $page['page_id']; ?>');" title="<?php echo $HEADING['ADD_CHILD_PAGE']; ?>">
 						<img src="<?php echo THEME_URL; ?>/images/siteadd.png" name="addpage_<?php echo $page['page_id']; ?>" alt="Add Child Page" />
 					</a>
 					<?php } ?>
