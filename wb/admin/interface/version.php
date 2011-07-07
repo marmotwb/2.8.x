45a4d78a4ca0c2202b979639c2cee32b807248c7
Luisehahne <Luisehahne@98bf1eab-f5fc-0310-a3ed-aac8851f7404>
2011-07-07 16:07:27 +0000

    change to Release Candiate 7

--- wb/admin/interface/version.php
+++ wb/admin/interface/version.php
@@ -51,7 +51,7 @@ if(!defined('WB_URL')) {
 }
 
 // check if defined to avoid errors during installation (redirect to admin panel fails if PHP error/warnings are enabled)
-if(!defined('VERSION')) define('VERSION', '2.8.2.RC6');
-if(!defined('REVISION')) define('REVISION', '1470');
+if(!defined('VERSION')) define('VERSION', '2.8.2.RC7');
+if(!defined('REVISION')) define('REVISION', '1471);
 
 ?>
