0d3f61e6b8aee5bb71d110be5430fe8e3a435f82
Luisehahne <Luisehahne@98bf1eab-f5fc-0310-a3ed-aac8851f7404>
2011-08-11 14:41:14 +0000

    comment out $email_body .= "\n\nIP: ".$_SERVER['REMOTE_ADDR'];

--- branches/2.8.x/wb/modules/form/view.php
+++ branches/2.8.x/wb/modules/form/view.php
@@ -401,8 +401,8 @@ if($filter_settings['email_filter'] && !($filter_settings['at_replacement']=='@'
 					/**	
 					 *	Adding the IP to the body and try to send the email
 					 */
-					$email_body .= "\n\nIP: ".$_SERVER['REMOTE_ADDR'];
-					
+					// $email_body .= "\n\nIP: ".$_SERVER['REMOTE_ADDR'];
+
 					if($email_to != '') {
 						if($email_from != '') {
 							if($wb->mail($email_from,$email_to,$email_subject,$email_body,$email_fromname)) {
