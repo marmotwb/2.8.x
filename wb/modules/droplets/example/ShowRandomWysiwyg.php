6d6233624bdb8bc77c83d869274c88c58b58ca23
darkviper <darkviper@98bf1eab-f5fc-0310-a3ed-aac8851f7404>
2012-02-25 00:29:49 +0000

    typofix in Droplet [ShowRandomWysiwyg]

--- wb/modules/droplets/example/ShowRandomWysiwyg.php
+++ wb/modules/droplets/example/ShowRandomWysiwyg.php
@@ -3,17 +3,15 @@
 global $database;
 	$content = '';
 	if (isset($section)) {
-		if( preg_match('/^[0-9]+(?:\s*\[\,\|\-\;\:\+\#\/]\s*[0-9]+\s*)*$/', $section)) {
+		if( preg_match('/^[0-9]+(?:\s*[\,\|\-\;\:\+\#\/]\s*[0-9]+\s*)*$/', $section)) {
 			if (is_readable(WB_PATH.'/modules/wysiwyg/view.php')) {
 			// if valid arguments given and module wysiwyg is installed
-				$iOldSectionId = intval($section_id); // save old SectionId
 				// split and sanitize arguments
 				$aSections = preg_split('/[\s\,\|\-\;\:\+\#\/]+/', $section);
-				$section_id = $sections[array_rand($sections)]; // get random element
+				$section_id = $aSections[array_rand($aSections)]; // get random element
 				ob_start(); // generate output by wysiwyg module
 				require(WB_PATH.'/modules/wysiwyg/view.php');
 				$content = ob_get_clean();
-				$section_id = $ioldSectionId; // restore old SectionId
 			}
 		}
 	}
