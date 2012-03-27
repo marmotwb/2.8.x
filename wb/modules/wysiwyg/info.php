040917cc75d29fd16496577078f41a57664b912b
Luisehahne <Luisehahne@98bf1eab-f5fc-0310-a3ed-aac8851f7404>
2012-03-27 10:35:26 +0000

    fixed path to globalExceptionHandler.php in wysiwyg

--- wb/modules/wysiwyg/info.php
+++ wb/modules/wysiwyg/info.php
@@ -7,7 +7,7 @@
  * @copyright       2009-2012, Website Baker Org. e.V.
  * @link			http://www.websitebaker2.org/
  * @license         http://www.gnu.org/licenses/gpl.html
- * @platform        WebsiteBaker 2.8.x
+ * @platform        WebsiteBaker 2.8.3
  * @requirements    PHP 5.2.2 and higher
  * @version         $Id$
  * @filesource		$HeadURL$
@@ -17,7 +17,7 @@
 /* -------------------------------------------------------- */
 // Must include code to stop this file being accessed directly
 if(!defined('WB_PATH')) {
-	require_once(dirname(dirname(__FILE__)).'/framework/globalExceptionHandler.php');
+	require_once(dirname(dirname(dirname(__FILE__))).'/framework/globalExceptionHandler.php');
 	throw new IllegalFileException();
 }
 /* -------------------------------------------------------- */
