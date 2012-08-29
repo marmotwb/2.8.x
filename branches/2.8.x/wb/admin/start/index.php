e478b7584af9206328687097965712ac486c70e0
Luisehahne <Luisehahne@98bf1eab-f5fc-0310-a3ed-aac8851f7404>
2012-08-29 11:56:10 +0000

    ! update folder admin/start, admin/admintools, admin/skel/htt

--- branches/2.8.x/wb/admin/start/index.php
+++ branches/2.8.x/wb/admin/start/index.php
@@ -4,7 +4,7 @@
  * @category        admin
  * @package         start
  * @author          Ryan Djurovich, WebsiteBaker Project
- * @copyright       2009-2011, Website Baker Org. e.V.
+ * @copyright       2009-2012, WebsiteBaker Org. e.V.
  * @link			http://www.websitebaker2.org/
  * @license         http://www.gnu.org/licenses/gpl.html
  * @platform        WebsiteBaker 2.8.x
@@ -13,12 +13,17 @@
  * @filesource		$HeadURL$
  * @lastmodified    $Date$
  *
-*/
-
+ */
+//$string = "pages,pages_view,pages_add,pages_add_l0,pages_settings,pages_modify,pages_intro,pages_delete,media,media_view,media_upload,media_rename,media_delete,media_create,addons,modules,modules_view,modules_install,modules_uninstall,templates,templates_view,templates_install,templates_uninstall,languages,languages_view,languages_install,languages_uninstall,settings,settings_basic,settings_advanced,access,users,users_view,users_add,users_modify,users_delete,groups,groups_view,groups_add,groups_modify,groups_delete,admintools
+//media,media_create,media_upload,media_view,preferences,preferences_view,pages,pages_modify,pages_view";
+//$regex = "/(pages)+[a-z]*[_]([a-z_0-9]+)[^,]/im";
+//preg_match_all ($regex, $string, $output);
+//
 require('../../config.php');
 require_once(WB_PATH.'/framework/class.admin.php');
 $admin = new admin('Start','start');
 // ---------------------------------------
+//	$database = WbDatabase::getInstance();
 
 if(defined('FINALIZE_SETUP')) {
 	require_once(WB_PATH.'/framework/functions.php');
@@ -52,8 +57,12 @@ if(defined('FINALIZE_SETUP')) {
 	if($database->query($sql)) { }
 }
 // ---------------------------------------
-$msg = '<br />';
 // check if it is neccessary to start the uograde-script
+$msg = '';
+
+// ---------------------------------------
+// check if it is neccessary to start the uograde-script
+// ---------------------------------------
 if(($admin->get_user_id()==1) && file_exists(WB_PATH.'/upgrade-script.php')) {
 	// check if it is neccessary to start the uograde-script
 	$sql = 'SELECT `value` FROM `'.TABLE_PREFIX.'settings` WHERE `name`=\'wb_revision\'';
@@ -72,64 +81,167 @@ if(($admin->get_user_id()==1) && file_exists(WB_PATH.'/upgrade-script.php')) {
 	}
 	$msg .= ''.$MESSAGE['START_UPGRADE_SCRIPT_EXISTS'].'<br />';
 }
+// ---------------------------------------
+// workout to upgrade the groups system_permissions
+// ---------------------------------------
+if( ($admin->get_user_id()==1) &&
+	file_exists(ADMIN_PATH.'/groups/upgradePermissions.php') &&
+	!defined('GROUPS_UPDATED') )
+{
+	// check if it is neccessary to start the uograde-script
+	$sql = 'SELECT `value` FROM `'.TABLE_PREFIX.'settings` WHERE `name`=\'wb_revision\'';
+	if($wb_revision = $database->get_one($sql)) {
 
+	}
+	if (version_compare($wb_revision, '1691' ) < 0) {
+		require_once (ADMIN_PATH.'/groups/upgradePermissions.php');
+		// build new or changed $sTempPermissions
+		if(upgrade_group_system_permissions()){
+			$cfg = array(
+				'groups_updated' => time()
+			);
+			if(db_update_key_value( 'settings', $cfg )) {
+			    echo "<div class=\"note center rounded\"><h3>Hello Systemadministrator!</h3>".
+				     "<p style=\"text-align:center;\">".
+				     "The <strong>Administrator Groups Rights </strong> were updated automatically.\n" .
+			         "Please click <a style=\"font-weight:bold;\" " .
+			         "href=\"".ADMIN_URL."/logout/index.php\">on this link</a> to login again!</p>\n".
+			         "<h3>Upgrading only start, if groups rights are not up to date!</h3></div>";
+				// Print admin footer
+				$admin->print_footer();
+			    exit(0);
+
+			}
+		}
+	}
+}
+
+// ---------------------------------------
 // Setup template object, parse vars to it, then parse it
 // Create new template object
-$template = new Template(dirname($admin->correct_theme_source('start.htt')));
-$template->set_file('page', 'start.htt');
-$template->set_block('page', 'main_block', 'main');
+$oTpl = new Template(dirname($admin->correct_theme_source('start.htt')),'keep');
+$oTpl->set_file('page', 'start.htt');
+$oTpl->set_block('page', 'main_block', 'main');
 
 // Insert values into the template object
-$template->set_var(array(
-					'WELCOME_MESSAGE' => $MESSAGE['START']['WELCOME_MESSAGE'],
-					'CURRENT_USER' => $MESSAGE['START']['CURRENT_USER'],
+$oTpl->set_var(array(
+					'WELCOME_MESSAGE' => $MESSAGE['START_WELCOME_MESSAGE'],
+					'CURRENT_USER' => $MESSAGE['START_CURRENT_USER'],
 					'DISPLAY_NAME' => $admin->get_display_name(),
 					'ADMIN_URL' => ADMIN_URL,
 					'WB_URL' => WB_URL,
 					'THEME_URL' => THEME_URL,
-					'WB_VERSION' => WB_VERSION
+					'WB_VERSION' => WB_VERSION,
+					'NO_CONTENT' => ''
 				)
 			);
 
+
 // Insert permission values into the template object
+$oTpl->set_block('main_block', 'show_pages_block', 'show_pages');
 if($admin->get_permission('pages') != true)
 {
-	$template->set_var('DISPLAY_PAGES', 'display:none;');
+	$oTpl->set_block('show_pages', '');
+} else {
+	$oTpl->parse('show_pages', 'show_pages_block', true);
+}
+
+$oTpl->set_block('main_block', 'show_addons_block', 'show_addons');
+if($admin->get_permission('addons') != true)
+{
+	$oTpl->set_block('show_addons', '');
+} else {
+	$oTpl->parse('show_addons', 'show_addons_block', true);
+}
+
+$oTpl->set_block('main_block', 'show_settings_block', 'show_settings');
+if($admin->get_permission('settings') != true)
+{
+	$oTpl->set_block('show_settings', '');
+} else {
+	$oTpl->parse('show_settings', 'show_settings_block', true);
+}
+
+$oTpl->set_block('main_block', 'show_access_block', 'show_access');
+if($admin->get_permission('access') != true)
+{
+	$oTpl->set_block('show_access', '');
+} else {
+	$oTpl->parse('show_access', 'show_access_block', true);
+}
+
+$oTpl->set_block('main_block', 'show_media_block', 'show_media');
+if($admin->get_permission('media') != true)
+{
+	$oTpl->set_block('show_media', '');
+} else {
+	$oTpl->parse('show_media', 'show_media_block', true);
+}
+
+$oTpl->set_block('main_block', 'show_admintools_block', 'show_admintools');
+if($admin->get_permission('admintools') != true)
+{
+	$oTpl->set_block('show_admintools', '');
+} else {
+	$oTpl->parse('show_admintools', 'show_admintools_block', true);
+}
+
+$oTpl->set_block('main_block', 'show_preferences_block', 'show_preferences');
+if($admin->get_permission('preferences') != true)
+{
+	$oTpl->set_block('show_preferences', '');
+} else {
+	$oTpl->parse('show_preferences', 'show_preferences_block', true);
+}
+
+$oTpl->set_block('main_block', 'show_install_block', 'show_install');
+if($admin->ami_group_member('1') != true)
+{
+	$oTpl->set_block('show_install', '');
+} else {
+	$oTpl->parse('show_install', 'show_install_block', true);
+}
+
+/*
+if($admin->get_permission('pages') != true)
+{
+	$oTpl->set_var('DISPLAY_PAGES', 'display:none;');
 }
 if($admin->get_permission('media') != true)
 {
-	$template->set_var('DISPLAY_MEDIA', 'display:none;');
+	$oTpl->set_var('DISPLAY_MEDIA', 'display:none;');
 }
 if($admin->get_permission('addons') != true)
 {
-	$template->set_var('DISPLAY_ADDONS', 'display:none;');
+	$oTpl->set_var('DISPLAY_ADDONS', 'display:none;');
 }
 if($admin->get_permission('access') != true)
 {
-	$template->set_var('DISPLAY_ACCESS', 'display:none;');
+	$oTpl->set_var('DISPLAY_ACCESS', 'display:none;');
 }
 if($admin->get_permission('settings') != true)
 {
-	$template->set_var('DISPLAY_SETTINGS', 'display:none;');
+	$oTpl->set_var('DISPLAY_SETTINGS', 'display:none;');
 }
 if($admin->get_permission('admintools') != true)
 {
-	$template->set_var('DISPLAY_ADMINTOOLS', 'display:none;');
+	$oTpl->set_var('DISPLAY_ADMINTOOLS', 'display:none;');
 }
+*/
 
-$msg .= (file_exists(WB_PATH.'/install/')) ?  $MESSAGE['START']['INSTALL_DIR_EXISTS'] : '';
+$msg .= (file_exists(WB_PATH.'/install/')) ?  $MESSAGE['START_INSTALL_DIR_EXISTS'] : '';
 
 // Check if installation directory still exists
 if(file_exists(WB_PATH.'/install/') || file_exists(WB_PATH.'/upgrade-script.php') ) {
 	// Check if user is part of Adminstrators group
 	if(in_array(1, $admin->get_groups_id()))
     {
-		$template->set_var('WARNING', $msg );
+		$oTpl->set_var('WARNING', $msg );
 	} else {
-		$template->set_var('DISPLAY_WARNING', 'display:none;');
+		$oTpl->set_var('DISPLAY_WARNING', 'display:none;');
 	}
 } else {
-	$template->set_var('DISPLAY_WARNING', 'display:none;');
+	$oTpl->set_var('DISPLAY_WARNING', 'display:none;');
 }
 
 // Insert "Add-ons" section overview (pretty complex compared to normal)
@@ -166,7 +278,7 @@ if($admin->get_permission('groups') == true) {
 }
 
 // Insert section names and descriptions
-$template->set_var(array(
+$oTpl->set_var(array(
 					'PAGES' => $MENU['PAGES'],
 					'MEDIA' => $MENU['MEDIA'],
 					'ADDONS' => $MENU['ADDONS'],
@@ -186,8 +298,8 @@ $template->set_var(array(
 			);
 
 // Parse template object
-$template->parse('main', 'main_block', false);
-$template->pparse('output', 'page');
+$oTpl->parse('main', 'main_block', false);
+$oTpl->pparse('output', 'page');
 
 // Print admin footer
 $admin->print_footer();
