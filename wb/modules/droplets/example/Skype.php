b2eb42085a86eeca35653bb1ef78d035fd70f13d
Luisehahne <Luisehahne@98bf1eab-f5fc-0310-a3ed-aac8851f7404>
2012-08-30 02:19:59 +0000

    # fixed missing class.admin # check droplet skype if no existing user

--- wb/modules/droplets/example/Skype.php
+++ wb/modules/droplets/example/Skype.php
@@ -1,3 +1,4 @@
 //:Your skype status as an image
 //:Commandline to use: [[skype?user=skypename]]
+$user = (isset($user) && ($user!='') ? $user : '');
 return '<img src="http://mystatus.skype.com/'.$user.'.png?t='.time().'" alt="My Skype status" />';
