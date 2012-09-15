bd93d45c0c7070688a269af419de91a364758970
Luisehahne <Luisehahne@98bf1eab-f5fc-0310-a3ed-aac8851f7404>
2012-09-15 20:25:07 +0000

    ! update wysiwyg install.php, now using methode SqlImport from WBDatabase

---
+++ branches/2.8.x/wb/modules/wysiwyg/sql/mod_wysiwyg.sql
@@ -0,0 +1,37 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.5
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Erstellungszeit: 15. Sep 2012 um 21:37
+-- Server Version: 5.5.16
+-- PHP-Version: 5.3.8
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `mod_wysiwyg`
+--
+
+DROP TABLE IF EXISTS `{TABLE_PREFIX}mod_wysiwyg`;
+CREATE TABLE IF NOT EXISTS `{TABLE_PREFIX}mod_wysiwyg` (
+  `wysiwyg_id` int(11) NOT NULL AUTO_INCREMENT,
+  `section_id` int(11) NOT NULL DEFAULT '0',
+  `page_id` int(11) NOT NULL DEFAULT '0',
+  `content` longtext NOT NULL,
+  `text` longtext NOT NULL,
+  PRIMARY KEY (`wysiwyg_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
