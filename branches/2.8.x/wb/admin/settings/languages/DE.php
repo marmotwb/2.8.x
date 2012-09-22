ab9a2a67e2e7f2ee9108fcb0f5c4e72495799f85
Luisehahne <Luisehahne@98bf1eab-f5fc-0310-a3ed-aac8851f7404>
2012-09-22 20:30:37 +0000

    ! change page_extended to extended settings under page language

--- branches/2.8.x/wb/admin/settings/languages/DE.php
+++ branches/2.8.x/wb/admin/settings/languages/DE.php
@@ -8,5 +8,9 @@
 
 $TEXT['WEBSITE_SIGNATURE'] = 'Kontakt Signature';
 $TEXT['PAGE_EXTENDED'] = 'Erweiterte Seiteneinstellungen';
-$TEXT['CONFIRMED_REGISTRATION'] = 'Aktivierungs-Mail';
+$TEXT['CONFIRMED_REGISTRATION'] = 'Registrierung mit Aktivierungs-Mail';
+$TEXT['REBUILD_ACCESS_FILES'] = 'Zugriffsdateien aktualiseren';
+$TEXT['MODULES_DIRECTORY'] = 'Module für Aktualisierung';
 $HELP['ACTIVATION_MAIL'] = 'Registrierung per Aktivierungs-Mail (Eingeschaltet) oder Direktregistrierung (Ausgeschaltet)';
+$HELP['REBUILD_ACCESS_FILES'] = '- Aktualisiert auch nachfolgend eingetragene geprüfte Module, (Komma getrennte Liste)';
+//
--- branches/2.8.x/wb/admin/settings/languages/EN.php
+++ branches/2.8.x/wb/admin/settings/languages/EN.php
@@ -7,5 +7,9 @@
 
 $TEXT['WEBSITE_SIGNATURE'] = 'Contact Signature';
 $TEXT['PAGE_EXTENDED'] = 'Extended Pagesettings';
-$TEXT['CONFIRMED_REGISTRATION'] = 'Activation-Mail';
+$TEXT['CONFIRMED_REGISTRATION'] = 'Register with Activation-Mail';
+$TEXT['REBUILD_ACCESS_FILES'] = 'Rebuild Accessfiles';
+$TEXT['MODULES_DIRECTORY'] = 'Modules for upgrade';
 $HELP['ACTIVATION_MAIL'] = 'Registration per Activation-Mail (Enabled) or Directregistration (Disabled)';
+$HELP['REBUILD_ACCESS_FILES'] = '- Modules listet in the next field, will be upgraded too, (Comma separate list)';
+//
