6dae893a242da69601e2b89037042d53e86ab422
darkviper <darkviper@98bf1eab-f5fc-0310-a3ed-aac8851f7404>
2013-02-19 11:40:12 +0000

    update classes Translate, TranslateTable, TranslateAdaptorWbOldStyle added interface TranslateAdaptorInterface added Klasse_Translate_de.pdf update class WbAdaptor update /framework/initialize.php

---
+++ branches/2.8.x/wb/framework/TranslateAdaptorInterface.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * TranslateAdaptorInterface.php
+ *
+ * @category     Core
+ * @package      Core_Translation
+ * @author       Werner v.d.Decken <wkl@isteam.de>
+ * @copyright    Werner v.d.Decken <wkl@isteam.de>
+ * @license      http://www.gnu.org/licenses/gpl.html   GPL License
+ * @version      0.0.1
+ * @revision     $Revision$
+ * @link         $HeadURL$
+ * @lastmodified $Date$
+ * @since        File available since 06.02.2013
+ * @description  definitions for all TranslateAdaptorXxxxx
+ */
+interface TranslateAdaptorInterface {
+	
+	public function __construct($sAddon = '');
+	public function loadLanguage($sLangCode);
+	public function findFirstLanguage();
+	
+} // end of interface TranslateAdaptorInterface
+
--- branches/2.8.x/wb/framework/initialize.php
+++ branches/2.8.x/wb/framework/initialize.php
@@ -1,25 +1,38 @@
 <?php
 /**
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * @category        framework
- * @package         initialize
- * @author          Ryan Djurovich, WebsiteBaker Project
- * @copyright       2009-2012, WebsiteBaker Org. e.V.
- * @link			http://www.websitebaker2.org/
- * @license         http://www.gnu.org/licenses/gpl.html
- * @platform        WebsiteBaker 2.8.x
- * @requirements    PHP 5.2.2 and higher
- * @version         $Id$
- * @filesource		$HeadURL$
- * @lastmodified    $Date$
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-/* -------------------------------------------------------- */
-// Must include code to stop this file being accessed directly
-require_once(dirname(__FILE__).'/globalExceptionHandler.php');
-if(!defined('WB_URL')) { throw new IllegalFileException(); }
-/* -------------------------------------------------------- */
+/**
+ * initialize.php
+ *
+ * @category     Core
+ * @package      Core_Environment
+ * @author       Werner v.d.Decken <wkl@isteam.de>
+ * @copyright    Werner v.d.Decken <wkl@isteam.de>
+ * @license      http://www.gnu.org/licenses/gpl.html   GPL License
+ * @version      0.0.1
+ * @revision     $Revision$
+ * @link         $HeadURL$
+ * @lastmodified $Date$
+ * @since        File replaced since 05.02.2013
+ * @description  set the basic environment to run WebsiteBaker
+ */
+
+/* *** define some helper functions *** */
 /**
  * sanitize $_SERVER['HTTP_REFERER']
  * @param string $sWbUrl qualified startup URL of current application
@@ -48,9 +61,12 @@ if(!defined('WB_URL')) { throw new IllegalFileException(); }
 		}
 		$_SERVER['HTTP_REFERER'] = $sTmpReferer;
 	}
-/* -------------------------------------------------------- */
+/**
+ * Set constants for system/install values
+ * @throws RuntimeException
+ */
 	function SetInstallPathConstants() {
-		if(!defined('DEBUG')){ define('DEBUG', false); }// Include config file
+		if(!defined('DEBUG')){ define('DEBUG', false); } // normaly set in config file
 		if(!defined('ADMIN_DIRECTORY')){ define('ADMIN_DIRECTORY', 'admin'); }
 		if(!preg_match('/xx[a-z0-9_][a-z0-9_\-\.]+/i', 'xx'.ADMIN_DIRECTORY)) {
 			throw new RuntimeException('Invalid admin-directory: ' . ADMIN_DIRECTORY);
@@ -62,135 +78,211 @@ if(!defined('WB_URL')) { throw new IllegalFileException(); }
 			$x1 = parse_url(WB_URL);
 			define('WB_REL', (isset($x1['path']) ? $x1['path'] : ''));
 		}
+		define('ADMIN_REL', WB_REL.'/'.ADMIN_DIRECTORY);
 		if(!defined('DOCUMENT_ROOT')) {
+			
 			define('DOCUMENT_ROOT', preg_replace('/'.preg_quote(WB_REL, '/').'$/', '', WB_PATH));
 		}
+		define('TMP_PATH', WB_PATH.'/temp');
 	}
-/* -------------------------------------------------------- */
+/**
+ * Read DB settings from configuration file
+ * @return string
+ * @throws RuntimeException
+ * 
+ */
+	function readConfiguration($sRetvalType = 'url') {
+		$x = debug_backtrace();
+		if(sizeof($x) != 0) { throw new RuntimeException('illegal function request!'); }
+		$aRetval = array();
+		$sSetupFile = dirname(dirname(__FILE__)).'/setup.ini.php';
+		if(is_readable($sSetupFile)) {
+			$aCfg = parse_ini_file($sSetupFile, true);
+			foreach($aCfg['Constants'] as $key=>$value) {
+				if($key == 'debug') { $value = filter_var($value, FILTER_VALIDATE_BOOLEAN); }
+				if(!defined(strtoupper($key))) { define(strtoupper($key), $value); }
+			}
+			$db = $aCfg['DataBase'];
+			$db['type'] = isset($db['type']) ? $db['type'] : 'mysql';
+			$db['user'] = isset($db['user']) ? $db['user'] : 'foo';
+			$db['pass'] = isset($db['pass']) ? $db['pass'] : 'bar';
+			$db['host'] = isset($db['host']) ? $db['host'] : 'localhost';
+			$db['port'] = isset($db['port']) ? $db['port'] : '3306';
+			$db['port'] = ($db['port'] != '3306') ? $db['port'] : '';
+			$db['name'] = isset($db['name']) ? $db['name'] : 'dummy';
+			$db['charset'] = isset($db['charset']) ? $db['charset'] : 'utf8';
+			$db['table_prefix'] = (isset($db['table_prefix']) ? $db['table_prefix'] : '');
+			define('TABLE_PREFIX', $db['table_prefix']);
+			if($sRetvalType == 'dsn') {
+				$aRetval[0] = $db['type'].':dbname='.$db['name'].';host='.$db['host'].';'
+				            . ($db['port'] != '' ? 'port='.(int)$db['port'].';' : '');
+				$aRetval[1] = array('CHARSET' => $db['charset'], 'TABLE_PREFIX' => $db['table_prefix']);
+				$aRetval[2] = array( 'user' => $db['user'], 'pass' => $db['pass']);
+			}else { // $sRetvalType == 'url'
+				$aRetval[0] = $db['type'].'://'.$db['user'].':'.$db['pass'].'@'
+				            . $db['host'].($db['port'] != '' ? ':'.$db['port'] : '').'/'.$db['name'];
+			}
+			unset($db, $aCfg);
+			return $sRetval;
+		}
+		throw new RuntimeException('unable to read setup.ini.php');
+	}
+/* ***************************************************************************************
+ * Start initialization                                                                  *
+ ****************************************************************************************/
+// initialize debug evaluation values ---	
+	$sDbConnectType = 'url'; // depending from class WbDatabase it can be 'url' or 'dsn'
 	$starttime = array_sum(explode(" ",microtime()));
-	$iPhpDeclaredClasses =  sizeof(get_declared_classes());
+	$iPhpDeclaredClasses = sizeof(get_declared_classes());
+// disable all kind of magic_quotes in PHP versions before 5.4 ---
+	if(version_compare(PHP_VERSION, '5.4.0', '<')) {
+		if(get_magic_quotes_gpc() || get_magic_quotes_runtime()) {
+			@ini_set('magic_quotes_sybase', 0);
+			@ini_set('magic_quotes_gpc', 0);
+			@ini_set('magic_quotes_runtime', 0);
+		}
+	}
+// define constant systemenvironment settings ---
 	SetInstallPathConstants();
-	SanitizeHttpReferer(WB_URL); // sanitize $_SERVER['HTTP_REFERER']
-	if(!class_exists('WbAutoloader', false)){ include(dirname(__FILE__).'/WbAutoloader.php'); }
-	WbAutoloader::doRegister(array(ADMIN_DIRECTORY=>'a', 'modules'=>'m'));
-	require_once dirname(dirname(__FILE__)).'/include/Twig/Autoloader.php';
-	Twig_Autoloader::register();
 	date_default_timezone_set('UTC');
-	// Create database class
-	$sSqlUrl = DB_TYPE.'://'.DB_USERNAME.':'.DB_PASSWORD.'@'.DB_HOST.'/'.DB_NAME;
+	if(!defined('MAX_TIME')) { define('MAX_TIME', (pow(2, 31)-1)); } // 32-Bit Timestamp of 19 Jan 2038 03:14:07 GMT
+	if(!defined('DO_NOT_TRACK')) { define('DO_NOT_TRACK', (isset($_SERVER['HTTP_DNT']))); }
+// register WB basic autoloader ---
+	$sTmp = dirname(__FILE__).'/WbAutoloader.php';
+	if(!class_exists('WbAutoloader', false)){ include($sTmp); }
+	WbAutoloader::doRegister(array(ADMIN_DIRECTORY=>'a', 'modules'=>'m'));
+// register TWIG autoloader ---
+//	$sTmp = dirname(dirname(__FILE__)).'/include/Sensio/Twig/lib/Twig/Autoloader.php';
+	$sTmp = dirname(dirname(__FILE__)).'/include/Twig/Autoloader.php';
+	if(!class_exists('Twig_Autoloader', false)){ include($sTmp); }
+	Twig_Autoloader::register();
+// aktivate exceptionhandler ---
+	if(!function_exists('globalExceptionHandler')) {
+		include(dirname(__FILE__).'/globalExceptionHandler.php');
+	}
+// load db configuration ---
+	if(defined('DB_TYPE')) {
+		$aSqlData = array( 0 => DB_TYPE.'://'.DB_USERNAME.':'.DB_PASSWORD.'@'.DB_HOST.'/'.DB_NAME);
+	}else {
+		$aSqlData = readConfiguration($sDbConnectType);
+	}
+// sanitize $_SERVER['HTTP_REFERER'] ---
+	SanitizeHttpReferer(WB_URL); 
+// ---------------------------
+// Create global database instance ---
 	$database = WbDatabase::getInstance();
-	$database->doConnect($sSqlUrl);
-	// disable all kind of magic_quotes
-	if(get_magic_quotes_gpc() || get_magic_quotes_runtime()) {
-		@ini_set('magic_quotes_sybase', 0);
-		@ini_set('magic_quotes_gpc', 0);
-		@ini_set('magic_quotes_runtime', 0);
+	if($sDbConnectType == 'dsn') {
+		$database->doConnect($aSqlData[0], $aSqlData[1]['user'], $aSqlData[1]['pass'], $aSqlData[2]);
+	}else {
+		$database->doConnect($aSqlData[0], TABLE_PREFIX);
 	}
-	// Get website settings (title, keywords, description, header, and footer)
-	$query_settings = "SELECT name,value FROM ".TABLE_PREFIX."settings";
-	$get_settings = $database->query($query_settings);
-	if($database->is_error()) { die($database->get_error()); }
-	if($get_settings->numRows() == 0) { die("Settings not found"); }
-	while($setting = $get_settings->fetchRow()) {
-		$setting_name=strtoupper($setting['name']);
-		$setting_value=$setting['value'];
-//		if ($setting_value=='') {$setting_value=false;}
-		if ($setting_value=='false'){$setting_value=false;}
-		if ($setting_value=='true'){$setting_value=true;}
-		@define($setting_name,$setting_value);
-	}
-	@define('DO_NOT_TRACK', (isset($_SERVER['HTTP_DNT'])));
-	$string_file_mode = STRING_FILE_MODE;
+	unset($aSqlData);
+// load global settings from database and define global consts from ---
+	$sql = 'SELECT `name`, `value` FROM `'.TABLE_PREFIX.'settings`';
+	if(($oSettings = $database->query($sql))) {
+		if(!$oSettings->numRows()) { throw new AppException('no settings found'); }
+		while($aSetting = $oSettings->fetchRow(MYSQL_ASSOC)) {
+			//sanitize true/false values
+			$aSetting['value'] = ($aSetting['value'] == 'true' 
+								  ? true 
+								  : ($aSetting['value'] == 'false' 
+									 ? false 
+									 : $aSetting['value'])
+								 );
+			// make global const from setting
+			@define(strtoupper($aSetting['name']), $aSetting['value']);
+		}
+	}else { throw new AppException($database->get_error()); }
+// get/set users timezone ---
+	define('TIMEZONE',    (isset($_SESSION['TIMEZONE'])    ? $_SESSION['TIMEZONE']    : DEFAULT_TIMEZONE));
+	define('DATE_FORMAT', (isset($_SESSION['DATE_FORMAT']) ? $_SESSION['DATE_FORMAT'] : DEFAULT_DATE_FORMAT));
+	define('TIME_FORMAT', (isset($_SESSION['TIME_FORMAT']) ? $_SESSION['TIME_FORMAT'] : DEFAULT_TIME_FORMAT));
+// set Theme directory --- 
+	define('THEME_URL',  WB_URL.'/templates/'.DEFAULT_THEME);
+	define('THEME_PATH', WB_PATH.'/templates/'.DEFAULT_THEME);
+	define('THEME_REL',  WB_REL.'/templates/'.DEFAULT_THEME);
+// extended wb editor settings
+	define('EDIT_ONE_SECTION', false);
+	define('EDITOR_WIDTH', 0);
+// define numeric, octal values for chmod operations ---	
+	$string_file_mode = STRING_FILE_MODE; // STRING_FILE_MODE is set deprecated
 	define('OCTAL_FILE_MODE',(int) octdec($string_file_mode));
-	$string_dir_mode = STRING_DIR_MODE;
+	$string_dir_mode = STRING_DIR_MODE; // STRING_DIR_MODE is set deprecated
 	define('OCTAL_DIR_MODE',(int) octdec($string_dir_mode));
+// define form security class and preload it ---
 	$sSecMod = (defined('SECURE_FORM_MODULE') && SECURE_FORM_MODULE != '') ? '.'.SECURE_FORM_MODULE : '';
 	$sSecMod = WB_PATH.'/framework/SecureForm'.$sSecMod.'.php';
 	require_once($sSecMod);
-	if (!defined("WB_INSTALL_PROCESS")) {
-		// get CAPTCHA and ASP settings
-		$table = TABLE_PREFIX.'mod_captcha_control';
-		if( ($get_settings = $database->query("SELECT * FROM $table LIMIT 1")) ) {
-			if($get_settings->numRows() == 0) { die("CAPTCHA-Settings not found"); }
-			$setting = $get_settings->fetchRow();
-			if($setting['enabled_captcha'] == '1') define('ENABLED_CAPTCHA', true);
-			else define('ENABLED_CAPTCHA', false);
-			if($setting['enabled_asp'] == '1') define('ENABLED_ASP', true);
-			else define('ENABLED_ASP', false);
-			define('CAPTCHA_TYPE', $setting['captcha_type']);
-			define('ASP_SESSION_MIN_AGE', (int)$setting['asp_session_min_age']);
-			define('ASP_VIEW_MIN_AGE', (int)$setting['asp_view_min_age']);
-			define('ASP_INPUT_MIN_AGE', (int)$setting['asp_input_min_age']);
-		}
-	}
-
-	// set error-reporting
-	if(intval(ER_LEVEL) > 0 )
-	{
+// set error-reporting from loaded settings ---
+	if(intval(ER_LEVEL) > 0 ) {
 		error_reporting(ER_LEVEL);
-		if( intval(ini_get ( 'display_errors' )) == 0 )
-		{
+		if( intval(ini_get ( 'display_errors' )) == 0 ) {
 			ini_set('display_errors', 1);
 		}
 	}
-	// Start a session
+// Start a session ---
 	if(!defined('SESSION_STARTED')) {
 		session_name(APP_NAME.'_session_id');
 		@session_start();
 		define('SESSION_STARTED', true);
 	}
-	if(defined('ENABLED_ASP') && ENABLED_ASP && !isset($_SESSION['session_started']))
+// *** begin deprecated part *************************************************************
+// load settings for use in Captch and ASP module
+	if (!defined("WB_INSTALL_PROCESS")) {
+		$sql = 'SELECT * FROM `'.TABLE_PREFIX.'mod_captcha_control`';
+		// request settings from database
+		if(($oSettings = $database->query($sql))) {
+			if(($aSetting = $oSettings->fetchRow(MYSQL_ASSOC))) {
+				define('ENABLED_CAPTCHA', ($aSetting['enabled_captcha'] == '1'));
+				define('ENABLED_ASP', ($aSetting['enabled_asp'] == '1'));
+				define('CAPTCHA_TYPE', $aSetting['captcha_type']);
+				define('ASP_SESSION_MIN_AGE', (int)$aSetting['asp_session_min_age']);
+				define('ASP_VIEW_MIN_AGE', (int)$aSetting['asp_view_min_age']);
+				define('ASP_INPUT_MIN_AGE', (int)$aSetting['asp_input_min_age']);
+			}
+		}
+	}
+	if(defined('ENABLED_ASP') && ENABLED_ASP && !isset($_SESSION['session_started'])) {
 		$_SESSION['session_started'] = time();
-
-
-
-	// Get users language
-	$requestMethod = '_'.strtoupper($_SERVER['REQUEST_METHOD']);
-	$language = (intval(isset(${$requestMethod}['lang'])) ? ${$requestMethod}['lang'] : null);
-	if(isset($language) AND $language != '' AND !is_numeric($language) AND strlen($language) == 2) {
-		define('LANGUAGE', strtoupper($language));
+	}
+// *** end of deprecated part ************************************************************
+// get user language ---
+	$sRequestMethod = '_'.strtoupper($_SERVER['REQUEST_METHOD']);
+	// check if get/post value is available
+	$sTempLanguage = (isset(${$sRequestMethod}['lang']) ? ${$sRequestMethod}['lang'] : '');
+	// validate language code
+	if(preg_match('/^[a-z]{2}$/si', $sTempLanguage)) {
+	// if there's valid get/post
+		define('LANGUAGE', strtoupper($sTempLanguage));
 		$_SESSION['LANGUAGE']=LANGUAGE;
-	} else {
-		if(isset($_SESSION['LANGUAGE']) AND $_SESSION['LANGUAGE'] != '') {
+	}else {
+		if(isset($_SESSION['LANGUAGE']) && $_SESSION['LANGUAGE']) {
+		// if there's valid session value
 			define('LANGUAGE', $_SESSION['LANGUAGE']);
-		} else {
+		}else {
+		// otherwise set to default
 			define('LANGUAGE', DEFAULT_LANGUAGE);
 		}
 	}
-
-	// Load Language file
-	if(!defined('LANGUAGE_LOADED')) {
-		if(!file_exists(WB_PATH.'/languages/'.LANGUAGE.'.php')) {
-			exit('Error loading language file '.LANGUAGE.', please check configuration');
-		} else {
-			require_once(WB_PATH.'/languages/'.LANGUAGE.'.php');
-		}
-	}
-
-	// Get users timezone
-	if(isset($_SESSION['TIMEZONE'])) {
-		define('TIMEZONE', $_SESSION['TIMEZONE']);
+// activate translations / load language definitions
+/** begin of deprecated part || will be replaced by class Translate **/	
+// Load Language file
+	if(!file_exists(WB_PATH.'/languages/'.LANGUAGE.'.php')) {
+		$sMsg = 'Error loading language file '.LANGUAGE.', please check configuration';
+		throw new AppException($sMsg);
 	} else {
-		define('TIMEZONE', DEFAULT_TIMEZONE);
+	// include language file
+		require_once(WB_PATH.'/languages/'.LANGUAGE.'.php');
 	}
-	// Get users date format
-	if(isset($_SESSION['DATE_FORMAT'])) {
-		define('DATE_FORMAT', $_SESSION['DATE_FORMAT']);
-	} else {
-		define('DATE_FORMAT', DEFAULT_DATE_FORMAT);
+/** end of deprecated part **/
+// instantiate and initialize adaptor for temporary registry replacement ---
+	if(class_exists('WbAdaptor')) {
+		WbAdaptor::getInstance()->getWbConstants();
 	}
-	// Get users time format
-	if(isset($_SESSION['TIME_FORMAT'])) {
-		define('TIME_FORMAT', $_SESSION['TIME_FORMAT']);
-	} else {
-		define('TIME_FORMAT', DEFAULT_TIME_FORMAT);
-	}
-
-	// Set Theme dir
-	define('THEME_URL', WB_URL.'/templates/'.DEFAULT_THEME);
-	define('THEME_PATH', WB_PATH.'/templates/'.DEFAULT_THEME);
-
-	// extended wb_settings
-	define('EDIT_ONE_SECTION', false);
-
-	define('EDITOR_WIDTH', 0);
+// load and activate new global translation table
+	Translate::getInstance()->initialize('en',
+	                                     (defined('DEFAULT_LANGUAGE') ? DEFAULT_LANGUAGE : ''), 
+	                                     (defined('LANGUAGE') ? LANGUAGE : ''), 
+	                                     'WbOldStyle');
+// *** END OF FILE ***********************************************************************
+	
