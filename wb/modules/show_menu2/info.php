408c2e1f6f0e8fe8284207f6010c171947211991
Luisehahne <Luisehahne@98bf1eab-f5fc-0310-a3ed-aac8851f7404>
2013-06-07 04:12:10 +0000

    ! /modules/show_menu2/  set to version 4.9.7   DEBUG == true show query counts

--- wb/modules/show_menu2/info.php
+++ wb/modules/show_menu2/info.php
@@ -3,15 +3,15 @@
  *
  * @category        module
  * @package         show_menu2
+ * @author          Brodie Thiesfield
  * @author          WebsiteBaker Project
- * @copyright       2004-2009, Ryan Djurovich
- * @copyright       2009-2011, Website Baker Org. e.V.
- * @link			http://www.websitebaker2.org/
+ * @copyright       2009-2013, WebsiteBaker Org. e.V.
+ * @link            http://www.websitebaker.org/
  * @license         http://www.gnu.org/licenses/gpl.html
- * @platform        WebsiteBaker 2.7.0
+ * @platform        WebsiteBaker 2.8.4
  * @requirements    PHP 5.2.2 and higher
  * @version         $Id$
- * @filesource		$HeadURL$
+ * @filesource      $HeadURL$
  * @lastmodified    $Date$
  *
  */
@@ -19,9 +19,8 @@
 $module_directory = 'show_menu2';
 $module_name = 'show_menu2';
 $module_function = 'snippet';
-$module_version = '4.9.6';
-$module_platform = '2.7 | 2.8.2';
+$module_version = '4.9.7';
+$module_platform = '2.8.4';
 $module_author = 'Brodie Thiesfield';
 $module_license = 'GNU General Public License';
 $module_description = 'A code snippet for the Website Baker CMS providing a complete replacement for the builtin menu functions. See <a href="http://code.jellycan.com/show_menu2/" target="_blank">http://code.jellycan.com/show_menu2/</a> for details or view the <a href="' .WB_URL .'/modules/show_menu2/README.en.txt" target="_blank">readme</a> file.';
-
