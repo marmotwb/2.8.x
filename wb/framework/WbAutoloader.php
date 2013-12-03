509563e7105559e8399dc083b30c39393592b43a
darkviper <darkviper@98bf1eab-f5fc-0310-a3ed-aac8851f7404>
2013-12-03 15:56:05 +0000

    ! WbAutoloader added possibility to load classes with additional file prefix 'class.'

--- wb/framework/WbAutoloader.php
+++ wb/framework/WbAutoloader.php
@@ -16,11 +16,14 @@ class WbAutoloader
  */
 	static public function doRegister(array $aDirectories)
 	{
-		if(!sizeof(self::$_aSearchpatterns)) {
-			if(sizeof($aDirectories > 0)) {
+		if(!sizeof(self::$_aSearchpatterns))
+		{
+			if(sizeof($aDirectories > 0))
+			{
 				self::$_aSearchpatterns[] = '/(^.[^_].*$)/i';
-				self::$_aReplacements[] = basename(dirname(__FILE__)).'_$1';
-				foreach($aDirectories as $value => $shortKey) {
+				self::$_aReplacements[] = basename(dirname(__FILE__)).'_\1';
+				foreach($aDirectories as $value => $shortKey)
+				{
 					self::$_aSearchpatterns[] = '/^'.$shortKey.'_/i';
 					self::$_aReplacements[] = $value.'_';
 				}
@@ -34,15 +37,14 @@ class WbAutoloader
  */
 	static public function CoreAutoloader($sClassName)
 	{
-		if($sClassName == 'database'){
-			$sFileName = dirname(__FILE__).'/class.database.php';
-			if(is_file($sFileName)) { include($sFileName); }
-		}else {
-			$sClassName = preg_replace(self::$_aSearchpatterns, self::$_aReplacements, $sClassName);
-			$sFileName = dirname(dirname(__FILE__)).'/'.str_replace('_', '/', $sClassName).'.php';
-			if(is_file($sFileName = dirname(dirname(__FILE__)).'/'.str_replace('_', '/', $sClassName).'.php')) {
-				include($sFileName);
+		$sClassName = preg_replace(self::$_aSearchpatterns, self::$_aReplacements, $sClassName);
+		$sFileName = dirname(dirname(__FILE__)).'/'.str_replace('_', '/', $sClassName).'.php';
+		if (!is_file($sFileName = dirname(dirname(__FILE__)).'/'.str_replace('_', '/', $sClassName).'.php')) {
+		// alternatively search for file with prefix 'class.'
+			if (!is_file(($sFileName = dirname($sFileName).'/class.'.basename($sFileName)))) {
+				return false;
 			}
 		}
+		include($sFileName);
 	}
 } // end class Autoloader
