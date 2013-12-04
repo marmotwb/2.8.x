14370047d9f776d6ad894a0111ddd3a385173e38
darkviper <darkviper@98bf1eab-f5fc-0310-a3ed-aac8851f7404>
2013-12-04 18:08:53 +0000

    + added new Interface usable to connect from WYSIWYG-Editor to all Modules with subitems like news or topics

--- branches/2.8.x/wb/framework/WbLinkAbstract.php
+++ branches/2.8.x/wb/framework/WbLinkAbstract.php
@@ -45,7 +45,61 @@ abstract class WbLinkAbstract {
 	}
 
 	abstract public function makeLinkFromTag(array $aReplacement);
-//	abstract public function generateLinkList(Template $oTpl);
+	abstract public function &generateOptionsList();
+
+/**
+ *
+ * @param string $sAddonName
+ * @param string $sAddonTableName
+ * @param string $sPageIdName
+ * @param string $sSectionIdName
+ * @param string $sItemIdName
+ * @param string $sDateTimeName
+ * @param string $sTitleName
+ * @param string $sActivatedName
+ * @return array by reference
+ */
+	final protected function &_executeListGeneration(
+	                                                  $sAddonName      = 'news',
+	                                                  $sAddonTableName = 'mod_news_posts',
+	                                                  $sPageIdName     = 'page_id',
+	                                                  $sSectionIdName  = 'section_id',
+	                                                  $sItemIdName     = 'post_id',
+	                                                  $sDateTimeName   = 'created_when',
+	                                                  $sTitleName      = 'title',
+	                                                  $sActivatedName  = 'active'
+	                                                )
+	{
+		$aAddonItems = array();
+		//generate news lists
+		$sql = 'SELECT `p`.`'.$sItemIdName.'` `ItemId`, `p`.`'.$sPageIdName.'` `PageId`, '
+		     .        '`s`.`section_id` `SectionId`, `p`.`'.$sTitleName.'` `Title` '
+		     . 'FROM `'.$this->oDb->TablePrefix.'sections` `s` '
+			 . 'LEFT JOIN `'.$this->oDb->TablePrefix.$sAddonTableName.'` `p` ON `s`.`section_id`= `p`.`'.$sSectionIdName.'` '
+			 . 'WHERE `p`.`'.$sActivatedName.'`>0 AND `s`.`module`=\''.$sAddonName.'\' '
+			 . 'ORDER BY `s`.`page_id`, `s`.`section_id`, `p`.`'.$sDateTimeName.'` DESC';
+		if (( $oRes = $this->oDb->doQuery($sql))) {
+			$iCurrentPage    = 0;
+			$iCurrentSection = 0;
+			$iSectionCounter = 0;
+			while ($aItem = $oRes->fetchRow(MYSQL_ASSOC)) {
+				if ($iCurrentPage != $aItem['PageId']) {
+					$iCurrentPage = $aItem['PageId'];
+					$aAddonItems[$iCurrentPage.'P'] = array();
+				}
+				if ($iCurrentSection != $aItem['SectionId']) {
+					$iCurrentSection = $aItem['SectionId'];
+					$aAddonItems[$iCurrentPage.'P'][] = array();
+					$iSectionCounter = sizeof($aAddonItems[$iCurrentPage.'P'])-1;
+				}
+				$aAddonItems[$iCurrentPage.'P'][$iSectionCounter][] = array(
+				    'wblink' => '[wblink'.$aItem['PageId'].'?addon='.$sAddonName.'&item='.$aItem['ItemId'].']',
+				    'title'  => preg_replace("/\r?\n/", "\\n", $this->oDb->escapeString($aItem['Title']))
+				);
+			}
+		}
+		return $aAddonItems;
+	}
 
 
 } // end of class WbLinkAbstract
