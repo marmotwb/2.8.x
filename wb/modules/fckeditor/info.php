6596a1f2befdb1aa3af51527ee0a4061ed64b95e
darkviper <darkviper@98bf1eab-f5fc-0310-a3ed-aac8851f7404>
2013-12-16 20:53:31 +0000

    # script injection vulnerability fixed in fck-spellchecker

--- wb/modules/fckeditor/info.php
+++ wb/modules/fckeditor/info.php
@@ -8,7 +8,7 @@
  * @copyright       2009-2011, Website Baker Org. e.V.
  * @link			http://www.websitebaker2.org/
  * @license         http://www.gnu.org/licenses/gpl.html
- * @platform        WebsiteBaker 2.8.x
+ * @platform        WebsiteBaker 2.8.4
  * @requirements    PHP 5.2.2 and higher
  * @version         $Id$
  * @filesource		$HeadURL$
@@ -20,8 +20,9 @@ $module_directory		= 'fckeditor';
 $module_name			= 'FCKeditor';
 $module_function		= 'WYSIWYG';
 $module_version			= '2.9.9';
-$module_platform		= '2.7 | 2.8.x';
-$module_requirements    = 'PHP 4.3.11 or higher, WB 2.7 or higher';
-$module_author 			= 'Christian Sommer, P. Widlund, S. Braunewell, M. Gallas, Wouldlouper, Aldus, Luisehahne';
+$module_platform		= '2.8.4';
+$module_requirements    = 'PHP 5.2.2 or higher, WB 2.8.4 or higher';
+$module_author 			= 'Christian Sommer, P. Widlund, S. Braunewell, M. Gallas, Wouldlouper, Aldus, Luisehahne, M.v.d.Decken';
 $module_license 		= 'GNU General Public License';
-$module_description 	= 'This module allows you to edit the contents of a page using <a href="http://www.fckeditor.net/" target="_blank">FCKeditor v2.6.6</a>.';
+$module_description 	= 'This addon allows you to edit the contents of a page using <a href="http://www.fckeditor.net/" target="_blank">FCKeditor v2.6.6</a>.<br />'
+                        . 'This addon needs WB-2.8.4 and up to work!!';
