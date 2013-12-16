9503ad036ef25df348927cda565f41c011c853f8
Luisehahne <Luisehahne@98bf1eab-f5fc-0310-a3ed-aac8851f7404>
2013-12-16 02:59:35 +0000

    ! /modules/droplets/example/ update some droplets and remove the old ones

---
+++ wb/modules/droplets/example/iShowWysiwyg.php
@@ -0,0 +1,20 @@
+//:Display one defined WYSIWYG section
+//:Use [[iShowWysiwyg?section=10]]
+global $database, $section_id, $module;
+$content = '';
+$section = isset($section) ? intval($section) : 0;
+if ($section) {
+	if (is_readable(WB_PATH.'/modules/wysiwyg/view.php')) {
+		// if valid section is given and module wysiwyg is installed
+		$iOldSectionId = intval($section_id); // save old SectionID
+		$section_id = $section;
+		ob_start(); // generate output by regulary wysiwyg module
+		$sectionAnchor = (defined('SEC_ANCHOR') && SEC_ANCHOR!='') ? SEC_ANCHOR : 'Sec';
+		echo PHP_EOL.'<div id="'.$sectionAnchor.$section_id.'" class="section m_'.$module.'" >'.PHP_EOL;
+		require(WB_PATH.'/modules/wysiwyg/view.php');
+		echo PHP_EOL.'</div><!-- '.$module.$section_id.' -->'.PHP_EOL;
+		$content = ob_get_clean();
+		$section_id = $iOldSectionId; // restore old SectionId
+	}
+}
+return $content;
