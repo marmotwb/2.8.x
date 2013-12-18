f982a2a971ec63f0f96a86104f63bff2e8a58775
Luisehahne <Luisehahne@98bf1eab-f5fc-0310-a3ed-aac8851f7404>
2013-12-18 21:46:44 +0000

    + /admin/addons/languages/ add NL Languages File (Tks To Nibz)

---
+++ branches/2.8.x/wb/admin/addons/languages/NL.php
@@ -0,0 +1,95 @@
+<?php
+/****************************************************************************************
+ * extended language definition for WebsiteBaker                                        *
+ * ACP-module  pages                                                                    *
+ * Nederlands (NL)                                                                      *
+ ****************************************************************************************/
+
+
+$HEADING['CHANGE_TEMPLATE_NOTICE'] = 'Klik <i>Geavanceerd</i> om het huidige Website-beheertemplate te kopieren!';
+$HEADING['INSTALL_LANGUAGE'] = 'Toevoegen taalbestand';
+$HEADING['INSTALL_MODULE'] = 'Toevoegen module';
+$HEADING['INSTALL_TEMPLATE'] = 'Toevoegen template';
+$HEADING['INVOKE_LANGUAGE_FILES'] = 'Taal-bestanden handmatig inschakelen';
+$HEADING['INVOKE_MODULE_FILES'] = 'Handmatige module-installatie';
+$HEADING['INVOKE_TEMPLATE_FILES'] = 'Template-bestanden handmatig inschakelen';
+$HEADING['LANGUAGE_DETAILS'] = 'Taalbestandgegevens';
+$HEADING['MODULE_DETAILS'] = 'Modulegegevens';
+$HEADING['TEMPLATE_DETAILS'] = 'Templategegevens';
+$HEADING['UNINSTALL_LANGUAGE'] = 'Verwijderen taalbestand';
+$HEADING['UNINSTALL_MODULE'] = 'Verwijderen module';
+$HEADING['UNINSTALL_TEMPLATE'] = 'Verwijderen temlate';
+$HEADING['UPGRADE_LANGUAGE'] = 'Upgraden/inschakelen taalbestand';
+$HEADING['UPLOAD_FILES'] = 'Uploaden bestanden';
+
+$MENU['LANGUAGES'] = 'Talen';
+$MENU['MODULES'] = 'Modules';
+$MENU['TEMPLATES'] = 'Templates';
+
+$MESSAGE['ADDON_ERROR_RELOAD'] = 'Fout tijdens het updaten van de add-onbestanden.';
+$MESSAGE['ADDON_LANGUAGES_RELOADED'] = 'Taalbestanden succesvol herladen';
+$MESSAGE['ADDON_MANUAL_FTP_LANGUAGE'] = '<strong>ATTENTIE!</strong> Om veiligheidsredenen dient u de taalbestanden in de folder /languages/ via FTP te uploaden en vervolgens de Upgrade-functie te gebruiken om ze aan het systeem toe te voegen.';
+$MESSAGE['ADDON_MANUAL_FTP_WARNING'] = 'Waarschuwing: bestaande module database-informatie zal verloren gaan! ';
+$MESSAGE['ADDON_MANUAL_INSTALLATION'] = 'When modules are uploaded via FTP (not recommended), the module installation functions <tt>install</tt>, <tt>upgrade</tt> or <tt>uninstall</tt> will not be executed automatically. Those modules may not work correct or do not uninstall properly.<br /><br />You can execute the module functions manually for modules uploaded via FTP below.';
+$MESSAGE['ADDON_MANUAL_INSTALLATION_WARNING'] = 'Waarschuwing: de bestaande module database-informatie zal verloren gaan! Gebruik deze optie alleen indien u ervaring heeft met modules die zijn geupload via FTP.';
+$MESSAGE['ADDON_MANUAL_RELOAD_WARNING'] = 'Waarschuwing: de bestaande module database-informatie zal verloren gaan! ';
+$MESSAGE['ADDON_MODULES_RELOADED'] = 'Modules succesvol herladen';
+$MESSAGE['ADDON_OVERWRITE_NEWER_FILES'] = 'Nieuwere bestanden overschrijven';
+$MESSAGE['ADDON_PRECHECK_FAILED'] = 'Add-on installatie mislukt. Uw systeem voldoet niet aan de eisen van deze add-on. Om deze situatie te veranderen kunt u de informatie hieronder toepassen.';
+$MESSAGE['ADDON_RELOAD'] = 'Update database met informatie uit de add-on bestanden (bijvoorbeeld na FTP-upload).';
+$MESSAGE['ADDON_TEMPLATES_RELOADED'] = 'Templates succesvol herladen';
+$MESSAGE['ADMIN_INSUFFICIENT_PRIVELLIGES'] = 'Onvoldoende rechten om hier te zijn';
+$MESSAGE['GENERIC_ALREADY_INSTALLED'] = 'Is al ge&iuml;nstalleerd';
+$MESSAGE['GENERIC_BAD_PERMISSIONS'] = 'Kan niet schrijven naar doelmap';
+$MESSAGE['GENERIC_CANNOT_UNINSTALL'] = 'Kan niet de&iuml;nstalleren';
+$MESSAGE['GENERIC_CANNOT_UNINSTALL_IN_USE'] = 'Kan niet de&iuml;nstalleren: het geselecteerde bestand is in gebruik';
+$MESSAGE['GENERIC_CANNOT_UNINSTALL_IN_USE_TMPL'] = '<br /><br />De {{type}} <b>{{type_name}}</b> kan niet verwijderd worden omdat het in gebruik is op {{pages}}:<br /><br />';
+$MESSAGE['GENERIC_CANNOT_UNINSTALL_IN_USE_TMPL_PAGES'] = 'volgende pagina;volgende pagina\'s';
+$MESSAGE['GENERIC_CANNOT_UNINSTALL_IS_DEFAULT_TEMPLATE'] = 'De template <b>{{name}}</b> kan niet verwijderd worden omdat het de standaard-template is.';
+$MESSAGE['GENERIC_CANNOT_UNINSTALL_IS_DEFAULT_THEME'] = 'Het is niet mogelijk de template <b>{{name}}</b> te verwijderen omdat het het standaard admin-thema is.';
+$MESSAGE['GENERIC_CANNOT_UNZIP'] = 'Kan het bestand niet uitpakken';
+$MESSAGE['GENERIC_CANNOT_UPLOAD'] = 'Kan niet uploaden';
+$MESSAGE['GENERIC_FORGOT_OPTIONS'] = 'U heeft niets geselecteerd!';
+$MESSAGE['GENERIC_INSTALLED'] = 'Installatie voltooid';
+$MESSAGE['GENERIC_INVALID'] = 'Ongeldig bestand';
+$MESSAGE['GENERIC_INVALID_ADDON_FILE'] = 'Ongeldig WebsiteBaker installatiebestand. Controleer het *.zip bestand.';
+$MESSAGE['GENERIC_INVALID_LANGUAGE_FILE'] = 'Ongeldig WebsiteBaker taalbestand. Controleer het tekstbestand.';
+$MESSAGE['GENERIC_INVALID_MODULE_FILE'] = 'Ongeldig WebsiteBaker modulebestand. Controleer het tekstbestand';
+$MESSAGE['GENERIC_INVALID_TEMPLATE_FILE'] = 'Ongeldig WebsiteBaker template-bestand. Controleer het tekstbestand';
+$MESSAGE['GENERIC_IN_USE'] = ' gebruikt in ';
+$MESSAGE['GENERIC_MISSING_ARCHIVE_FILE'] = 'Ontbrekend archiefbestand!';
+$MESSAGE['GENERIC_MODULE_VERSION_ERROR'] = 'De module is niet juist ge&iuml;nstalleerd!';
+$MESSAGE['GENERIC_NOT_INSTALLED'] = 'Niet ge&iuml;nstalleerd';
+$MESSAGE['GENERIC_NOT_UPGRADED'] = 'Actualisatie niet mogelijk';
+$MESSAGE['GENERIC_SECURITY_ACCESS'] = 'Veiligheidsrisico! Toegang geweigerd!';
+$MESSAGE['GENERIC_SECURITY_OFFENSE'] = 'Veiligheidsrisico! Data-opslag geweigerd!';
+$MESSAGE['GENERIC_UNINSTALLED'] = 'De&iuml;nstallatie voltooid';
+$MESSAGE['GENERIC_UPGRADED'] = 'Upgrade voltooid';;
+$MESSAGE['MEDIA_BLANK_NAME'] = 'U heeft geen nieuwe naam opgegeven';
+$MESSAGE['MEDIA_CANNOT_DELETE_DIR'] = 'Kan geselecteerde map niet verwijderen';
+
+$TEXT['ADVANCED'] = 'Geavanceerd';
+$TEXT['ARE_YOU_SURE'] = 'Weet u het zeker?';
+$TEXT['AUTHOR'] = 'Auteur';
+$TEXT['BACK'] = 'Terug';
+$TEXT['CANCEL'] = 'Annuleren';
+$TEXT['CODE'] = 'Code';
+$TEXT['COMMENT'] = 'Reageren';
+$TEXT['CURRENT_FOLDER'] = 'Huidige map';
+$TEXT['DESCRIPTION'] = 'Metatag "Description';
+$TEXT['DESIGNED_FOR'] = 'Ontworpen voor';
+$TEXT['EXECUTE'] = 'Uitvoeren';
+$TEXT['FILE'] = 'Bestand';
+$TEXT['FUNCTION'] = 'Functie';
+$TEXT['INSTALL'] = 'Installeren';
+$TEXT['LICENSE'] = 'License';
+$TEXT['NAME'] = 'Naam';
+$TEXT['PLEASE_SELECT'] = 'Selecteer';
+$TEXT['RESET'] = 'Opnieuw';
+$TEXT['SAVE'] = 'Opslaan';
+$TEXT['TEMPLATE'] = 'Template';
+$TEXT['THEME'] = 'Thema Website-beheer';
+$TEXT['TYPE'] = 'Type';
+$TEXT['UNINSTALL'] = 'Verwijderen';
+$TEXT['VIEW_DETAILS'] = 'Gegevens bekijken';
+$TEXT['VERSION'] = 'Versie';
