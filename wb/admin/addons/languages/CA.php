43e570f0babfb1f91601f0e53230bfc86e02decb
Luisehahne <Luisehahne@98bf1eab-f5fc-0310-a3ed-aac8851f7404>
2013-12-19 03:03:44 +0000

    + /admin/addons/languages/ add more Language Files (Tks To jacobi22)

---
+++ wb/admin/addons/languages/CA.php
@@ -0,0 +1,94 @@
+<?php
+/****************************************************************************************
+ * extended language definition for WebsiteBaker                                        *
+ * ACP-module  pages                                                                    *
+ * Catalan (CA)                                                                             *
+ ****************************************************************************************/
+
+
+$HEADING['CHANGE_TEMPLATE_NOTICE'] = 'Click <i>Advanced</i> to copy topical Backend-Theme!';
+$HEADING['INSTALL_LANGUAGE'] = 'Instal·la Idioma';
+$HEADING['INSTALL_MODULE'] = 'Instal·la Mòdul';
+$HEADING['INSTALL_TEMPLATE'] = 'Instal·la Plantilla';
+$HEADING['INVOKE_LANGUAGE_FILES'] = 'Execute language files manually';
+$HEADING['INVOKE_MODULE_FILES'] = 'Execute module files manually';
+$HEADING['INVOKE_TEMPLATE_FILES'] = 'Execute template files manually';
+$HEADING['LANGUAGE_DETAILS'] = 'Detalls de l\'Idioma';
+$HEADING['MODULE_DETAILS'] = 'Detalls del Mòdul';
+$HEADING['TEMPLATE_DETAILS'] = 'Detalls de la Plantilla';
+$HEADING['UNINSTALL_LANGUAGE'] = 'Desinstal·la Idioma';
+$HEADING['UNINSTALL_MODULE'] = 'Desinstal·la Mòdul';
+$HEADING['UNINSTALL_TEMPLATE'] = 'Desinstal·la Plantilla';
+$HEADING['UPGRADE_LANGUAGE'] = 'Language register/upgrading';
+$HEADING['UPLOAD_FILES'] = 'Penja Fitxer(s)';
+
+$MENU['LANGUAGES'] = 'Idiomes';
+$MENU['MODULES'] = 'Mòduls';
+$MENU['TEMPLATES'] = 'Plantilles';
+
+$MESSAGE['ADDON_ERROR_RELOAD'] = 'Error while updating the Add-On information.';
+$MESSAGE['ADDON_LANGUAGES_RELOADED'] = 'Languages reloaded successfully';
+$MESSAGE['ADDON_MANUAL_FTP_LANGUAGE'] = '<strong>ATTENTION!</strong> For safety reasons uploading languages files in the folder/languages/ only by FTP and use the Upgrade function for registering or updating.';
+$MESSAGE['ADDON_MANUAL_FTP_WARNING'] = 'Warning: Existing module database entries will get lost. ';
+$MESSAGE['ADDON_MANUAL_INSTALLATION'] = 'When modules are uploaded via FTP (not recommended), the module installation functions <tt>install</tt>, <tt>upgrade</tt> or <tt>uninstall</tt> will not be executed automatically. Those modules may not work correct or do not uninstall properly.<br /><br />You can execute the module functions manually for modules uploaded via FTP below.';
+$MESSAGE['ADDON_MANUAL_INSTALLATION_WARNING'] = 'Warning: Existing module database entries will get lost. Only use this option if you experience problems with modules uploaded via FTP.';
+$MESSAGE['ADDON_MANUAL_RELOAD_WARNING'] = 'Warning: Existing module database entries will get lost. ';
+$MESSAGE['ADDON_MODULES_RELOADED'] = 'Modules reloaded successfully';
+$MESSAGE['ADDON_OVERWRITE_NEWER_FILES'] = 'Overwrite newer Files';
+$MESSAGE['ADDON_PRECHECK_FAILED'] = 'Add-on installation failed. Your system does not fulfill the requirements of this Add-on. To make this Add-on working on your system, please fix the issues summarized below.';
+$MESSAGE['ADDON_RELOAD'] = 'Update database with information from Add-on files (e.g. after FTP upload).';
+$MESSAGE['ADDON_TEMPLATES_RELOADED'] = 'Templates reloaded successfully';
+$MESSAGE['ADMIN_INSUFFICIENT_PRIVELLIGES'] = 'No teniu privilegis suficients per estar ací';
+$MESSAGE['GENERIC_ALREADY_INSTALLED'] = 'Ja està instal·lat';
+$MESSAGE['GENERIC_BAD_PERMISSIONS'] = 'No s\'ha pogut escriure al directori de destí';
+$MESSAGE['GENERIC_CANNOT_UNINSTALL'] = 'No s\'ha pogut desinstal·lar';
+$MESSAGE['GENERIC_CANNOT_UNINSTALL_IN_USE'] = 'No s\'ha pogut desinstal·lar: s\'està usant el fitxer seleccionat';
+$MESSAGE['GENERIC_CANNOT_UNINSTALL_IN_USE_TMPL'] = '<br /><br />{{type}} <b>{{type_name}}</b> could not be uninstalled, because it is still in use on {{pages}}.<br /><br />';
+$MESSAGE['GENERIC_CANNOT_UNINSTALL_IN_USE_TMPL_PAGES'] = 'this page;these pages';
+$MESSAGE['GENERIC_CANNOT_UNINSTALL_IS_DEFAULT_TEMPLATE'] = 'Can\'t uninstall the template <b>{{name}}</b>, because it is the default template!';
+$MESSAGE['GENERIC_CANNOT_UNINSTALL_IS_DEFAULT_THEME'] = 'Can\'t uninstall the template <b>{{name}}</b>, because it is the default backend theme!';
+$MESSAGE['GENERIC_CANNOT_UNZIP'] = 'No s\'ha pogut descomprimir el fitxer';
+$MESSAGE['GENERIC_CANNOT_UPLOAD'] = 'No s\'ha pogut penjar el fitxer';
+$MESSAGE['GENERIC_FORGOT_OPTIONS'] = 'You have selected no choice!';
+$MESSAGE['GENERIC_INSTALLED'] = 'Instal·lat amb èxit';
+$MESSAGE['GENERIC_INVALID'] = 'El fitxer que heu penjat no és vàlid';
+$MESSAGE['GENERIC_INVALID_ADDON_FILE'] = 'Invalid WebsiteBaker installation file. Please check the *.zip format.';
+$MESSAGE['GENERIC_INVALID_LANGUAGE_FILE'] = 'Invalid WebsiteBaker language file. Please check the text file.';
+$MESSAGE['GENERIC_INVALID_MODULE_FILE'] = 'Invalid WebsiteBaker module file. Please check the text file.';
+$MESSAGE['GENERIC_INVALID_TEMPLATE_FILE'] = 'Invalid WebsiteBaker template file. Please check the text file.';
+$MESSAGE['GENERIC_IN_USE'] = ' but used in ';
+$MESSAGE['GENERIC_MISSING_ARCHIVE_FILE'] = 'Missing Archiv file!';
+$MESSAGE['GENERIC_MODULE_VERSION_ERROR'] = 'The module is not installed properly!';
+$MESSAGE['GENERIC_NOT_INSTALLED'] = 'No està instal·lat';
+$MESSAGE['GENERIC_NOT_UPGRADED'] = 'Actualization not possibly';
+$MESSAGE['GENERIC_SECURITY_ACCESS'] = 'Security offense!! Access denied!';
+$MESSAGE['GENERIC_SECURITY_OFFENSE'] = 'Security offense!! data storing was refused!!';
+$MESSAGE['GENERIC_UNINSTALLED'] = 'Desinstal·lat amb èxit';
+$MESSAGE['GENERIC_UPGRADED'] = 'Upgraded successfully';
+$MESSAGE['MEDIA_CANNOT_DELETE_DIR'] = 'No es pot esborrar la carpeta seleccionada';
+
+$TEXT['ADVANCED'] = 'Avançat';
+$TEXT['ARE_YOU_SURE'] = 'Esteu segur?';
+$TEXT['AUTHOR'] = 'Autor';
+$TEXT['BACK'] = 'Arrere';
+$TEXT['CANCEL'] = 'Cancel·la';
+$TEXT['CODE'] = 'Codi';
+$TEXT['COMMENT'] = 'Comentari';
+$TEXT['CURRENT_FOLDER'] = 'Carpeta Actual';
+$TEXT['DESCRIPTION'] = 'Descripció';
+$TEXT['DESIGNED_FOR'] = 'Dissenyat Per';
+$TEXT['EXECUTE'] = 'Execució';
+$TEXT['FILE'] = 'fitxer';
+$TEXT['FUNCTION'] = 'Function';
+$TEXT['INSTALL'] = 'Instal·la';
+$TEXT['LICENSE'] = 'License';
+$TEXT['NAME'] = 'Nom';
+$TEXT['PLEASE_SELECT'] = 'Per favor trieu';
+$TEXT['RESET'] = 'Reinicia';
+$TEXT['SAVE'] = 'Desa';
+$TEXT['TEMPLATE'] = 'Plantilla';
+$TEXT['THEME'] = 'Backend-Theme';
+$TEXT['TYPE'] = 'Tipus';
+$TEXT['UNINSTALL'] = 'Desinstal·la';
+$TEXT['VIEW_DETAILS'] = 'Veure Detalls';
+$TEXT['VERSION'] = 'Versió';
---
+++ wb/admin/addons/languages/CS.php
@@ -0,0 +1,94 @@
+<?php
+/****************************************************************************************
+ * extended language definition for WebsiteBaker                                        *
+ * ACP-module  pages                                                                    *
+ * Čeština (CS)                                                                             *
+ ****************************************************************************************/
+
+
+$HEADING['CHANGE_TEMPLATE_NOTICE'] = 'Click <i>Advanced</i> to copy topical Backend-Theme!';
+$HEADING['INSTALL_LANGUAGE'] = 'Nainstalovat jazyk';
+$HEADING['INSTALL_MODULE'] = 'Nainstalovat modul';
+$HEADING['INSTALL_TEMPLATE'] = 'Nainstalovat šablonu';
+$HEADING['INVOKE_LANGUAGE_FILES'] = 'Execute language files manually';
+$HEADING['INVOKE_MODULE_FILES'] = 'Execute module files manually';
+$HEADING['INVOKE_TEMPLATE_FILES'] = 'Execute template files manually';
+$HEADING['LANGUAGE_DETAILS'] = 'Detaily jazyka';
+$HEADING['MODULE_DETAILS'] = 'Detaily modulu';
+$HEADING['TEMPLATE_DETAILS'] = 'Detaily šablony';
+$HEADING['UNINSTALL_LANGUAGE'] = 'Odinstalovat jazyk';
+$HEADING['UNINSTALL_MODULE'] = 'Odinstalovat modul';
+$HEADING['UNINSTALL_TEMPLATE'] = 'Odinstalovat šablonu';
+$HEADING['UPGRADE_LANGUAGE'] = 'Language register/upgrading';
+$HEADING['UPLOAD_FILES'] = 'Nahrát soubor(y)';
+
+$MENU['LANGUAGES'] = 'Jazyky';
+$MENU['MODULES'] = 'Moduly';
+$MENU['TEMPLATES'] = 'Šablony';
+
+$MESSAGE['ADDON_ERROR_RELOAD'] = 'Error while updating the Add-On information.';
+$MESSAGE['ADDON_LANGUAGES_RELOADED'] = 'Languages reloaded successfully';
+$MESSAGE['ADDON_MANUAL_FTP_LANGUAGE'] = '<strong>ATTENTION!</strong> For safety reasons uploading languages files in the folder/languages/ only by FTP and use the Upgrade function for registering or updating.';
+$MESSAGE['ADDON_MANUAL_FTP_WARNING'] = 'Warning: Existing module database entries will get lost. ';
+$MESSAGE['ADDON_MANUAL_INSTALLATION'] = 'When modules are uploaded via FTP (not recommended), the module installation functions <tt>install</tt>, <tt>upgrade</tt> or <tt>uninstall</tt> will not be executed automatically. Those modules may not work correct or do not uninstall properly.<br /><br />You can execute the module functions manually for modules uploaded via FTP below.';
+$MESSAGE['ADDON_MANUAL_INSTALLATION_WARNING'] = 'Warning: Existing module database entries will get lost. Only use this option if you experience problems with modules uploaded via FTP.';
+$MESSAGE['ADDON_MANUAL_RELOAD_WARNING'] = 'Warning: Existing module database entries will get lost. ';
+$MESSAGE['ADDON_MODULES_RELOADED'] = 'Modules reloaded successfully';
+$MESSAGE['ADDON_OVERWRITE_NEWER_FILES'] = 'Overwrite newer Files';
+$MESSAGE['ADDON_PRECHECK_FAILED'] = 'Add-on installation failed. Your system does not fulfill the requirements of this Add-on. To make this Add-on working on your system, please fix the issues summarized below.';
+$MESSAGE['ADDON_RELOAD'] = 'Update database with information from Add-on files (e.g. after FTP upload).';
+$MESSAGE['ADDON_TEMPLATES_RELOADED'] = 'Templates reloaded successfully';
+$MESSAGE['ADMIN_INSUFFICIENT_PRIVELLIGES'] = 'Nedostatečná oprávnění';
+$MESSAGE['GENERIC_ALREADY_INSTALLED'] = 'Již dříve nainstalováno';
+$MESSAGE['GENERIC_BAD_PERMISSIONS'] = 'Nelze zapisovat do cílového adresáře';
+$MESSAGE['GENERIC_CANNOT_UNINSTALL'] = 'Nelze odinstalovat';
+$MESSAGE['GENERIC_CANNOT_UNINSTALL_IN_USE'] = 'Nelze odinstalovat: soubor je právě používán';
+$MESSAGE['GENERIC_CANNOT_UNINSTALL_IN_USE_TMPL'] = '<br /><br />{{type}} <b>{{type_name}}</b> could not be uninstalled, because it is still in use on {{pages}}.<br /><br />';
+$MESSAGE['GENERIC_CANNOT_UNINSTALL_IN_USE_TMPL_PAGES'] = 'this page;these pages';
+$MESSAGE['GENERIC_CANNOT_UNINSTALL_IS_DEFAULT_TEMPLATE'] = 'Can\'t uninstall the template <b>{{name}}</b>, because it is the default template!';
+$MESSAGE['GENERIC_CANNOT_UNINSTALL_IS_DEFAULT_THEME'] = 'Can\'t uninstall the template <b>{{name}}</b>, because it is the default backend theme!';
+$MESSAGE['GENERIC_CANNOT_UNZIP'] = 'Nelze rozbalit (rozzipovat) soubor';
+$MESSAGE['GENERIC_CANNOT_UPLOAD'] = 'Nelze nahrát soubor';
+$MESSAGE['GENERIC_FORGOT_OPTIONS'] = 'You have selected no choice!';
+$MESSAGE['GENERIC_INSTALLED'] = 'Instalace proběhla úspěšně';
+$MESSAGE['GENERIC_INVALID'] = 'Nahrávaný soubor je neplatný';
+$MESSAGE['GENERIC_INVALID_ADDON_FILE'] = 'Invalid WebsiteBaker installation file. Please check the *.zip format.';
+$MESSAGE['GENERIC_INVALID_LANGUAGE_FILE'] = 'Invalid WebsiteBaker language file. Please check the text file.';
+$MESSAGE['GENERIC_INVALID_MODULE_FILE'] = 'Invalid WebsiteBaker module file. Please check the text file.';
+$MESSAGE['GENERIC_INVALID_TEMPLATE_FILE'] = 'Invalid WebsiteBaker template file. Please check the text file.';
+$MESSAGE['GENERIC_IN_USE'] = ' but used in ';
+$MESSAGE['GENERIC_MISSING_ARCHIVE_FILE'] = 'Missing Archiv file!';
+$MESSAGE['GENERIC_MODULE_VERSION_ERROR'] = 'The module is not installed properly!';
+$MESSAGE['GENERIC_NOT_INSTALLED'] = 'Nenainstalováno';
+$MESSAGE['GENERIC_NOT_UPGRADED'] = 'Actualization not possibly';
+$MESSAGE['GENERIC_SECURITY_ACCESS'] = 'Security offense!! Access denied!Security offense!! Access denied!';
+$MESSAGE['GENERIC_SECURITY_OFFENSE'] = 'Security offense!! data storing was refused!!';
+$MESSAGE['GENERIC_UNINSTALLED'] = 'Odinstalace proběhla úspěšně';
+$MESSAGE['GENERIC_UPGRADED'] = 'Aktualizace proběhla úspěšně';
+$MESSAGE['MEDIA_CANNOT_DELETE_DIR'] = 'Nelze smazat vybraný adresář';
+
+$TEXT['ADVANCED'] = 'Pokročilý';
+$TEXT['ARE_YOU_SURE'] = 'Jste si jisti?';
+$TEXT['AUTHOR'] = 'Autor';
+$TEXT['BACK'] = 'Zpět';
+$TEXT['CANCEL'] = 'Zrušit';
+$TEXT['CODE'] = 'Kód jazyka';
+$TEXT['COMMENT'] = 'Komentář';
+$TEXT['CURRENT_FOLDER'] = 'Současný adresář';
+$TEXT['DESCRIPTION'] = 'Popis';
+$TEXT['DESIGNED_FOR'] = 'Vyvinuto pro';
+$TEXT['EXECUTE'] = 'Spuštění';
+$TEXT['FILE'] = 'soubor';
+$TEXT['FUNCTION'] = 'Function';
+$TEXT['INSTALL'] = 'Instalovat';
+$TEXT['LICENSE'] = 'License';
+$TEXT['NAME'] = 'Název';
+$TEXT['PLEASE_SELECT'] = 'Vyberte prosím';
+$TEXT['RESET'] = 'Vyčistit';
+$TEXT['SAVE'] = 'Uložit';
+$TEXT['TEMPLATE'] = 'Šablona';
+$TEXT['THEME'] = 'Backend-Theme';
+$TEXT['TYPE'] = 'Typ';
+$TEXT['UNINSTALL'] = 'Odinstalovat';
+$TEXT['VIEW_DETAILS'] = 'Zobrazit';
+$TEXT['VERSION'] = 'Verze';
