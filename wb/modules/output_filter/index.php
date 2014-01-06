a549e798b92679c2296c3bca8c35855f39c17595
darkviper <darkviper@98bf1eab-f5fc-0310-a3ed-aac8851f7404>
2014-01-06 22:46:12 +0000

    + added new outputfilter to use OutputFilterDashboard

--- wb/modules/output_filter/index.php
+++ wb/modules/output_filter/index.php
@@ -23,66 +23,52 @@ if(!defined('WB_PATH')) { throw new IllegalFileException(); }
 /* ************************************************************************** */
 /**
  * execute the frontend output filter to modify email addresses
- * @param string actual content
+ * @param string $sContent actual content
  * @return string modified content
  */
-	function executeFrontendOutputFilter($content) {
+	function executeFrontendOutputFilter($sContent) {
 		// get output filter settings from database
 		$filter_settings = getOutputFilterSettings();
-		$sFilterDirectory = str_replace('\\', '/', dirname(__FILE__)).'/filters/';
+		$sFilterDirectory = str_replace('\\', '/', __DIR__).'/filters/';
 		$output_filter_mode = 0;
 		$output_filter_mode |= ($filter_settings['email_filter'] * pow(2, 0));  // n | 2^0
 		$output_filter_mode |= ($filter_settings['mailto_filter'] * pow(2, 1)); // n | 2^1
 		define('OUTPUT_FILTER_AT_REPLACEMENT', $filter_settings['at_replacement']);
 		define('OUTPUT_FILTER_DOT_REPLACEMENT', $filter_settings['dot_replacement']);
+        if (!function_exists('OutputFilterApi')) {
+            require (__DIR__.'/OutputFilterApi.php');
+        }
+/* ### fixed default filters ################################################ */
+        $sContent = OutputFilterApi('ReplaceSysvar', $sContent);
+        $sContent = OutputFilterApi('WbLink', $sContent);
+/* ************************************************************************** */
+/* *** from here insert requests of individual variable filter ************** */
 
-/* ### filter type: replace Sysvar placeholder ############################## */
-		if (file_exists($sFilterDirectory.'filterReplaceSysvar.php')) {
-			require_once($sFilterDirectory.'filterReplaceSysvar.php');
-			$content = doFilterReplaceSysvar($content);
-		}
-/* ### filter type: change [wblinkxx] into real URLs ######################## */
-		if (file_exists($sFilterDirectory.'filterWbLink.php')) {
-			require_once($sFilterDirectory.'filterWbLink.php');
-			$content = doFilterWbLink($content);
-		}
 /* ### filter type: execute droplets filter ################################# */
-		if (file_exists($sFilterDirectory.'filterDroplets.php')) {
-			require_once($sFilterDirectory.'filterDroplets.php');
-			$content = doFilterDroplets($content);
-		}
+        $sContent = OutputFilterApi('Droplets', $sContent);
 /* ### filter type: protect email addresses ################################# */
 		if( ($output_filter_mode & pow(2, 0)) || ($output_filter_mode & pow(2, 1)) ) {
 			if (file_exists($sFilterDirectory.'filterEmail.php')) {
 				require_once($sFilterDirectory.'filterEmail.php');
-				$content = doFilterEmail($content, $output_filter_mode);
+				$sContent = doFilterEmail($sContent, $output_filter_mode);
 			}
 		}
-/* ### filter type: replace Sysvar placeholder ############################## */
-		if (file_exists($sFilterDirectory.'filterReplaceSysvar.php')) {
-			require_once($sFilterDirectory.'filterReplaceSysvar.php');
-			$content = doFilterReplaceSysvar($content);
-		}
-/* ### filter type: change [wblinkxx] into real URLs ######################## */
-		if (file_exists($sFilterDirectory.'filterWbLink.php')) {
-			require_once($sFilterDirectory.'filterWbLink.php');
-			$content = doFilterWbLink($content);
-		}
-/* ### filter type: full qualified URLs to relative URLs##################### */
-        if($filter_settings['sys_rel'] == 1){
-			if (file_exists($sFilterDirectory.'filterRelUrl.php')) {
-				require_once($sFilterDirectory.'filterRelUrl.php');
-				$content = doFilterRelUrl($content);
-			}
-		}
-/* ### filter type: moves css definitions from <body> into <head> ########### */
-		if (file_exists($sFilterDirectory.'filterCssToHead.php')) {
-			require_once($sFilterDirectory.'filterCssToHead.php');
-			$content = doFilterCssToHead($content);
-		}
+/* ### filter type: make use of Thorn's OutputFilterDashboard ############### */
+        if($filter_settings['opf'] == 1){
+            $sContent = OutputFilterApi('OpF', $sContent);
+        }
 
+/* *** end of individual variable filter ************************************ */
+/* ************************************************************************** */
+/* ### fixed default filters ################################################ */
+        $sContent = OutputFilterApi('ReplaceSysvar', $sContent);
+        $sContent = OutputFilterApi('WbLink', $sContent);
+        if($filter_settings['sys_rel'] == 1){
+            $sContent = OutputFilterApi('RelUrl', $sContent);
+		}
+        $sContent = OutputFilterApi('CssToHead', $sContent);
 /* ### end of filters ####################################################### */
-		return $content;
+		return $sContent;
 	}
 /* ************************************************************************** */
 /**
@@ -93,24 +79,23 @@ if(!defined('WB_PATH')) { throw new IllegalFileException(); }
  * @return array contains all settings
  */
 	function getOutputFilterSettings() {
-		global $database, $admin;
+        $oDb = WbDatabase::getInstance();
 	// set default values
 		$settings = array(
 			'sys_rel'         => 0,
+            'opf'             => 0,
 			'email_filter'    => 0,
 			'mailto_filter'   => 0,
 			'at_replacement'  => '(at)',
 			'dot_replacement' => '(dot)'
 		);
 	// be sure field 'sys_rel' is in table
-		$database->field_add( TABLE_PREFIX.'mod_output_filter', 'sys_rel', 'INT NOT NULL DEFAULT \'0\' FIRST');
+		$oDb->field_add( $oDb->TablePrefix.'mod_output_filter', 'sys_rel', 'INT NOT NULL DEFAULT \'0\' FIRST');
 	// request settings from database
-		$sql = 'SELECT * FROM `'.TABLE_PREFIX.'mod_output_filter` ';
-		if(($res = $database->query($sql))) {
-			if(($rec = $res->fetchRow())) {
+		$sql = 'SELECT * FROM `'.$oDb->TablePrefix.'mod_output_filter` ';
+		if(($res = $oDb->doQuery($sql))) {
+			if(($rec = $res->fetchRow(MYSQL_ASSOC))) {
 				$settings = $rec;
-				$settings['at_replacement']  = $admin->strip_slashes($settings['at_replacement']);
-				$settings['dot_replacement'] = $admin->strip_slashes($settings['dot_replacement']);
 			}
 		}
 	// return array with filter settings
